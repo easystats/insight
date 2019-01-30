bc716dada7e89f0d2b8a4d5a1022446633a5561f
Daniel <mail@danielluedecke.de>
2019-01-30 21:03:48 +0100

    draft link_fun() as counterpart link_inverse()

---
+++ R/link_fun.R
@@ -0,0 +1,45 @@
+#' @title Get link-function from model object
+#' @name link_fun
+#'
+#' @description Returns the link-function from a model object.
+#'
+#' @inheritParams model_predictors
+#'
+#' @return A function, describing the link-function from a model-object.
+#'
+#' @examples
+#' # example from ?stats::glm
+#' counts <- c(18, 17, 15, 20, 10, 20, 25, 13, 12)
+#' outcome <- gl(3, 1, 9)
+#' treatment <- gl(3, 3)
+#' m <- glm(counts ~ outcome + treatment, family = poisson())
+#'
+#' link_fun(m)(.3)
+#' # same as
+#' log(.3)
+#'
+#' @export
+#' @importFrom stats family
+link_fun <- function(x, ...) {
+  UseMethod("link_fun")
+}
+
+
+#' @export
+link_fun.default <- function(x, ...) {
+  tryCatch(
+    {
+      # get model family
+      ff <- stats::family(x)
+
+      # return link function, if exists
+      if ("linkfun" %in% names(ff)) return(ff$linkfun)
+
+      # else, create link function from link-string
+      if ("link" %in% names(ff)) return(match.fun(ff$link))
+
+      NULL
+    },
+    error = function(x) { NULL }
+  )
+}
