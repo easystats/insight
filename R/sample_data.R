2b0ea08d616f248d1d9a23f69f55ab90d64f1249
Daniel <mail@danielluedecke.de>
2019-02-02 11:36:19 +0100

    remove datasets

--- R/sample_data.R
+++ R/sample_data.R
@@ -1,15 +1,4 @@
 #' @docType data
-#' @title Sample dataset from the National Health and Nutrition Examination Survey
-#' @name nhanes_sample
-#' @keywords data
-#'
-#' @description Selected variables from the National Health and Nutrition Examination
-#'              Survey that are used in the example from Lumley (2010), Appendix E.
-#'
-#' @references Lumley T (2010). Complex Surveys: a guide to analysis using R. Wiley
-NULL
-
-#' @docType data
 #' @title Sample dataset from the EUROFAMCARE project
 #' @name efc
 #' @keywords data
@@ -17,14 +6,4 @@ NULL
 #' @description German data set from the European study on family care of older people.
 #'
 #' @references Lamura G, Döhner H, Kofahl C, editors. Family carers of older people in Europe: a six-country comparative study. Münster: LIT, 2008.
-NULL
-
-#' @docType data
-#' @title Sample dataset
-#' @name fish
-#' @keywords data
-#'
-#' @description Sample data from the UCLA idre website.
-#'
-#' @references https://stats.idre.ucla.edu/r/dae/zip/
 NULL
--- tests/testthat/test-GLMMadaptive.R
+++ tests/testthat/test-GLMMadaptive.R
@@ -1,7 +1,6 @@
 if (require("testthat") && require("insight") && require("GLMMadaptive")) {
   context("insight, model_info")
 
-  # data(fish)
   fish <- read.csv("https://stats.idre.ucla.edu/stat/data/fish.csv")
   fish$nofish <- as.factor(fish$nofish)
   fish$livebait <- as.factor(fish$livebait)
