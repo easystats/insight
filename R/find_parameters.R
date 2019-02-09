12398c8a2cd50b1ca4b7f0091f8250bc69826d59
Dominique Makowski <dom.mak19@gmail.com>
2019-02-09 10:19:32 +0800

    Revert "find_/get_parameters"
    This reverts commit 78dc9e1ba712ea85f1d41183f496df4eb3945958.

--- R/find_parameters.R
+++
@@ -1,8 +0,0 @@
-#' find_parameters
-#' @importFrom stats coef
-#' @param model model
-#' @export
-find_parameters <- function(model) {
-  # TODO ENHANCE
-  return(names(coef(model)))
-}
--- R/get_parameters.R
+++
@@ -1,8 +0,0 @@
-#' get_parameters
-#' @importFrom stats coef
-#' @param model model
-#' @export
-get_parameters <- function(model) {
-  # TODO ENHANCE
-  return(as.data.frame(model)[find_parameters(model)])
-}
