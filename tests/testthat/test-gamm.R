e02a7b44e72b41c6d666091db3ee29a505848e16
Daniel <mail@danielluedecke.de>
2019-02-15 18:32:47 +0100

    add tests

---
+++ tests/testthat/test-gamm.R
@@ -0,0 +1,89 @@
+unloadNamespace("mgcv")
+unloadNamespace("gam")
+
+if (require("testthat") && require("insight") && require("mgcv")) {
+  context("insight, model_info")
+
+  dat <- gamSim(6, n = 200, scale = .2, dist = "poisson")
+  m1 <-
+    gamm(
+      y ~ s(x0) + s(x1) + s(x2),
+      family = poisson,
+      data = dat,
+      random = list(fac = ~ 1)
+    )
+
+  test_that("model_info", {
+    expect_true(model_info(m1)$is_poisson)
+  })
+
+  test_that("clean_names", {
+    expect_equal(clean_names(m1), c("y", "x0", "x1", "x2"))
+  })
+
+  test_that("find_predictors", {
+    expect_identical(find_predictors(m1), list(conditional = c("x0", "x1", "x2")))
+    expect_identical(find_predictors(m1, flatten = TRUE), c("x0", "x1", "x2"))
+    expect_null(find_predictors(m1, effects = "random"))
+  })
+
+  test_that("find_response", {
+    expect_identical(find_response(m1), "y")
+  })
+
+  test_that("get_response", {
+    expect_equal(get_response(m1), dat$y)
+  })
+
+  test_that("link_inverse", {
+    expect_equal(link_inverse(m1)(.2), exp(.2), tolerance = 1e-5)
+  })
+
+  test_that("get_data", {
+    expect_equal(nrow(get_data(m1)), 400)
+    expect_equal(colnames(get_data(m1)), c("y", "x0", "x1", "x2"))
+  })
+
+  test_that("find_formula", {
+    expect_length(find_formula(m1), 1)
+    expect_equal(
+      find_formula(m1),
+      list(conditional = as.formula("y ~ s(x0) + s(x1) + s(x2)"))
+    )
+  })
+
+  test_that("find_terms", {
+    expect_equal(find_terms(m1), list(response = "y", conditional = c("x0", "x1", "x2")))
+    expect_equal(find_terms(m1, flatten = TRUE), c("y", "x0", "x1", "x2"))
+  })
+
+  test_that("n_obs", {
+    expect_equal(n_obs(m1), 200)
+  })
+
+  test_that("linkfun", {
+    expect_false(is.null(link_function(m1)))
+  })
+
+  test_that("find_parameters", {
+    expect_equal(
+      find_parameters(m1),
+      list(
+        conditional = c(
+          "(Intercept)", sprintf("s(x0).%i", 1:9), sprintf("s(x1).%i", 1:9),
+          sprintf("s(x2).%i", 1:9)
+        )
+      ))
+    expect_equal(nrow(get_parameters(m1)), 28)
+    expect_equal(
+      get_parameters(m1)$parameter,
+      c(
+        "(Intercept)", sprintf("s(x0).%i", 1:9), sprintf("s(x1).%i", 1:9),
+        sprintf("s(x2).%i", 1:9)
+    ))
+  })
+
+  test_that("is_multivariate", {
+    expect_false(is_multivariate(m1))
+  })
+}
---
+++ tests/testthat/test-gamm4.R
@@ -0,0 +1,85 @@
+unloadNamespace("mgcv")
+unloadNamespace("gam")
+
+if (require("testthat") && require("insight") && require("gamm4")) {
+  context("insight, model_info")
+
+  set.seed(0)
+  dat <- gamSim(1, n = 400, scale = 2) ## simulate 4 term additive truth
+  dat$fac <- fac <- as.factor(sample(1:20, 400, replace = TRUE))
+  dat$y <- dat$y + model.matrix( ~ fac - 1) %*% rnorm(20) * .5
+
+  m1 <- gamm4(y ~ s(x0) + x1 + s(x2), data = dat, random =  ~ (1 | fac))
+
+  test_that("model_info", {
+    expect_true(model_info(m1)$is_linear)
+  })
+
+  test_that("clean_names", {
+    expect_equal(clean_names(m1), c("y", "x0", "x1", "x2"))
+  })
+
+  test_that("find_predictors", {
+    expect_identical(find_predictors(m1), list(conditional = c("x0", "x1", "x2")))
+    expect_identical(find_predictors(m1, flatten = TRUE), c("x0", "x1", "x2"))
+    expect_null(find_predictors(m1, effects = "random"))
+  })
+
+  test_that("find_response", {
+    expect_identical(find_response(m1), "y")
+  })
+
+  test_that("get_response", {
+    expect_equal(get_response(m1), dat$y)
+  })
+
+  test_that("link_inverse", {
+    expect_equal(link_inverse(m1)(.2), .2, tolerance = 1e-5)
+  })
+
+  test_that("get_data", {
+    expect_equal(nrow(get_data(m1)), 400)
+    expect_equal(colnames(get_data(m1)), c("y", "x0", "x1", "x2", "fac", "y.0", "Xr", "Xr.0", "X.(Intercept)", "X.x1", "X.s(x0)Fx1", "X.s(x2)Fx1"))
+  })
+
+  test_that("find_formula", {
+    expect_length(find_formula(m1), 1)
+    expect_equal(
+      find_formula(m1),
+      list(conditional = as.formula("y ~ s(x0) + s(x1) + s(x2)"))
+    )
+  })
+
+  test_that("find_terms", {
+    expect_equal(find_terms(m1), list(response = "y", conditional = c("x0", "x1", "x2")))
+    expect_equal(find_terms(m1, flatten = TRUE), c("y", "x0", "x1", "x2"))
+  })
+
+  test_that("n_obs", {
+    expect_equal(n_obs(m1), 400)
+  })
+
+  test_that("linkfun", {
+    expect_false(is.null(link_function(m1)))
+  })
+
+  test_that("find_parameters", {
+    expect_equal(
+      find_parameters(m1),
+      list(
+        conditional = c(
+          "(Intercept)", "x1", sprintf("s(x0).%i", 1:9), sprintf("s(x2).%i", 1:9)
+        )
+      ))
+    expect_equal(nrow(get_parameters(m1)), 20)
+    expect_equal(
+      get_parameters(m1)$parameter,
+      c(
+        "(Intercept)", "x1", sprintf("s(x0).%i", 1:9), sprintf("s(x2).%i", 1:9)
+    ))
+  })
+
+  test_that("is_multivariate", {
+    expect_false(is_multivariate(m1))
+  })
+}
