4984a2f0c4bc55b0ef46e5310cde11e9f6e3af1a
Daniel <mail@danielluedecke.de>
2019-03-16 16:48:48 +0100

    Update test-glmmTMB.R

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -246,13 +246,14 @@ if (require("testthat") && require("insight") && require("glmmTMB")) {
       var.dist = 0.41218,
       var.disp = 0
     ),
-    tolerance = 1e-4))
+    tolerance = 1e-5))
 
-    expect_equal(get_fixef_variance(m1), c(var.fixef = 0.3258869), tolerance = 1e-4)
-    expect_equal(get_ranef_variance(m1), c(var.ranef = 0.07842738), tolerance = 1e-4)
-    expect_equal(get_resid_variance(m1), c(var.resid = 0.41218), tolerance = 1e-4)
-    expect_equal(get_dist_variance(m1), c(var.dist = 0.41218), tolerance = 1e-4)
-    expect_equal(get_disp_variance(m1), c(var.disp = 0), tolerance = 1e-4)
+    expect_equal(get_fixef_variance(m1), c(var.fixef = 1.097124), tolerance = 1e-5)
+    expect_equal(get_ranef_variance(m1), c(var.ranef = 0.8671274), tolerance = 1e-5)
+    expect_warning(get_ranef_variance(m1), c(var.ranef = 0.8671274), tolerance = 1e-5)
+    expect_equal(get_resid_variance(m1), c(var.resid = 0.02634501 ), tolerance = 1e-5)
+    expect_equal(get_dist_variance(m1), c(var.dist = 0.02634501 ), tolerance = 1e-5)
+    expect_equal(get_disp_variance(m1), c(var.disp = 0), tolerance = 1e-5)
 
     expect_warning(get_variances(m5))
   })
