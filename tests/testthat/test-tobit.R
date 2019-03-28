149d72df379bd9bb52246e1acd5d44bd6e84244a
Daniel <mail@danielluedecke.de>
2019-03-29 00:13:08 +0100

    Update test-tobit.R

--- tests/testthat/test-tobit.R
+++ tests/testthat/test-tobit.R
@@ -79,8 +79,4 @@ if (require("testthat") && require("insight") && require("AER")) {
   test_that("is_multivariate", {
     expect_false(is_multivariate(m1))
   })
-
-  test_that("find_algorithm", {
-    expect_equal(find_algorithm(m1), list(algorithm = "ML"))
-  })
 }
