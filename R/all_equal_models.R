87709ddd2569779955c0a2c769278f0c4de486ef
Daniel <mail@danielluedecke.de>
2019-03-28 11:18:33 +0100

    give more info on verbose

--- R/all_equal_models.R
+++ R/all_equal_models.R
@@ -23,29 +23,39 @@
 #' all_models_equal(m1, m2)
 #' all_models_equal(m1, m2, m3)
 #' all_models_equal(m1, m4, m2, m3, verbose = TRUE)
+#' all_models_equal(m1, m4, mtcars, m2, m3, verbose = TRUE)
 #'
 #' @export
 all_models_equal <- function(..., verbose = FALSE) {
   objects <- list(...)
-  objects.names <- match.call(expand.dots = FALSE)$`...`
+  object_names <- match.call(expand.dots = FALSE)$`...`
 
   all_supported <- sapply(objects, is_model)
   all_classes <- sapply(objects, class)
   all_equal <- Reduce(identical, all_classes)
 
+  if (!all(all_supported) && verbose) {
+    differ <- which(!all_supported)
+
+    m1 <- "Following objects are no (supported) models:"
+    m2 <- paste0(sprintf("%s", object_names[differ]), collapse = ", ")
+
+    message(paste(m1, m2, collapse = " "))
+  }
+
   if (!all_equal && verbose) {
     differ <- which(!duplicated(all_classes))
 
     m1 <- sprintf(
-      "Following objects are not identical with %s (of class \"%s\"): ",
-      objects.names[1],
+      "Following objects are not identical with %s (of class \"%s\"):",
+      object_names[1],
       all_classes[[1]]
     )
 
     m2 <- paste0(
       sprintf(
         "%s (\"%s\")",
-        objects.names[differ[-1]],
+        object_names[differ[-1]],
         sapply(all_classes[differ[-1]], function(x) as.vector(x[[1]]))
       ),
       collapse = ", "
