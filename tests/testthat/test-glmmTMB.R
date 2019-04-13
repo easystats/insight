1ed0458c5094b039bdd94ab62f0a68aa5b27de00
Daniel <mail@danielluedecke.de>
2019-04-13 11:23:51 +0200

    Update test-glmmTMB.R

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -267,12 +267,14 @@ if (require("testthat") && require("insight") && require("glmmTMB")) {
 
     skip_on_cran()
 
-    m <- glmmTMB(
-      count ~ child + camper + (1 + xb| persons),
-      ziformula = ~ child + livebait + (1 + zg + nofish| ID),
-      dispformula = ~xb,
-      data = fish,
-      family = truncated_poisson()
+    expect_warning(
+      m <- glmmTMB(
+        count ~ child + camper + (1 + xb| persons),
+        ziformula = ~ child + livebait + (1 + zg + nofish| ID),
+        dispformula = ~xb,
+        data = fish,
+        family = truncated_poisson()
+      )
     )
 
     expect_equal(
