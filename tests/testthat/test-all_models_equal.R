0e53da3c3e178313f33d16f0d98e96d5b74f244f
Daniel <mail@danielluedecke.de>
2019-04-23 21:48:27 +0200

    add tests

---
+++ tests/testthat/test-all_models_equal.R
@@ -0,0 +1,21 @@
+if (require("testthat") && require("insight")) {
+  context("insight, all_models_equal")
+
+  library(lme4)
+  data(mtcars)
+  data(sleepstudy)
+
+  m1 <- lm(mpg ~ wt + cyl + vs, data = mtcars)
+  m2 <- lm(mpg ~ wt + cyl, data = mtcars)
+  m3 <- lmer(Reaction ~ Days + (1 | Subject), data = sleepstudy)
+  m4 <- glm(formula = vs ~ wt, family = binomial(), data = mtcars)
+
+  test_that("all_models_equal", {
+    expect_true(all_models_equal(m1, m2))
+    expect_false(all_models_equal(m1, m2, m3))
+    expect_message(expect_false(all_models_equal(m1, m4, m2, m3, verbose = TRUE)))
+
+    expect_true(is_model(m1))
+    expect_false(is_model(mtcars))
+  })
+}
