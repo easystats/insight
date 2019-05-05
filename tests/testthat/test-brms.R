98ad6f818cd33c813e64f119f53dd94cb24ec1b4
Daniel <mail@danielluedecke.de>
2019-05-05 12:57:41 +0200

    check brms on travis?

--- tests/testthat/test-brms.R
+++ tests/testthat/test-brms.R
@@ -1,7 +1,7 @@
 # .runThisTest <- Sys.getenv("RunAllinsightTests") == "yes"
 # if (.runThisTest && Sys.getenv("USER") != "travis") {
 
-if (Sys.getenv("USER") != "travis") {
+# if (Sys.getenv("USER") != "travis") {
   if (suppressWarnings(
     require("testthat") &&
       require("insight") &&
@@ -301,4 +301,4 @@ if (Sys.getenv("USER") != "travis") {
       ))
     })
   }
-}
+# }
