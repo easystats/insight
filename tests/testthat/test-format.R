7fa6fd6a87b6313a683d039d42d8eca2990b163e
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-06 14:31:04 +0200

    fix test issues

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -2,9 +2,8 @@ if (require("testthat") && require("insight")) {
   context("insight, format")
 
   test_that("format_value", {
-    testthat::expect_equal(nchar(format_value(1.2012313)), 4)
-    testthat::expect_true(is.int(2))
-    testthat::expect_equal(nchar(format_value(4.2, protect_integers = TRUE)), 4)
-    testthat::expect_equal(nchar(format_value(4.0, protect_integers = TRUE)), 1)
+    expect_equal(nchar(format_value(1.2012313)), 4)
+    expect_equal(format_value(4.2, protect_integers = TRUE), "4.20")
+    expect_equal(format_value(4.0, protect_integers = TRUE), "4")
   })
 }
