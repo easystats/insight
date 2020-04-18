96e17e393fa7d91d4082fe3644531af613612b48
Daniel <mail@danielluedecke.de>
2020-04-18 11:57:32 +0200

    fix tests for both mlogit versions

--- tests/testthat/test-mlogit.R
+++ tests/testthat/test-mlogit.R
@@ -48,22 +48,41 @@ if (require("testthat") &&
   test_that("get_data", {
     expect_equal(nrow(get_data(m1)), 4728)
     expect_equal(nrow(get_data(m2)), 4728)
-    expect_equal(
-      colnames(get_data(m1)),
-      c("mode", "price", "catch", "idx", "probabilities", "linpred")
-    )
-    expect_equal(
-      colnames(get_data(m2)),
-      c(
-        "mode",
-        "price",
-        "catch",
-        "income",
-        "idx",
-        "probabilities",
-        "linpred"
+
+    if (packageVersion("mlogit") <= "1.0-3.1") {
+      expect_equal(
+        colnames(get_data(m1)),
+        c("mode", "price", "catch", "probabilities", "linpred")
       )
-    )
+      expect_equal(
+        colnames(get_data(m2)),
+        c(
+          "mode",
+          "price",
+          "catch",
+          "income",
+          "probabilities",
+          "linpred"
+        )
+      )
+    } else {
+      expect_equal(
+        colnames(get_data(m1)),
+        c("mode", "price", "catch", "idx", "probabilities", "linpred")
+      )
+      expect_equal(
+        colnames(get_data(m2)),
+        c(
+          "mode",
+          "price",
+          "catch",
+          "income",
+          "idx",
+          "probabilities",
+          "linpred"
+        )
+      )
+    }
   })
 
   test_that("find_formula", {
