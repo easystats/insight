9e9a976c6230872c62d5d82359e73df8da271203
mattansb <35330040+mattansb@users.noreply.github.com>
2020-09-19 11:45:34 +0300

    added protect_ratio arg to format_bf()
    #226

--- R/format_bf.R
+++ R/format_bf.R
@@ -1,6 +1,7 @@
 #' Bayes Factor formatting
 #'
 #' @param bf Bayes Factor.
+#' @param protect_ratio Should values smaller than 1 be represented as ratios?
 #' @inheritParams format_p
 #'
 #' @return A formatted string.
@@ -10,19 +11,34 @@
 #' format_bf(c(1.20, 1557, 3.5, 12), stars = TRUE)
 #' format_bf(c(1.20, 1557, 3.5, 12), name = NULL)
 #' @export
-format_bf <- function(bf, stars = FALSE, stars_only = FALSE, name = "BF") {
-  text <- ifelse(bf > 999, "> 999***",
-    paste0(
-      "= ",
-      ifelse(bf > 30, paste0(format_value(bf), "***"),
-        ifelse(bf > 10, paste0(format_value(bf), "**"),
-          ifelse(bf > 3, paste0(format_value(bf), "*"),
-            paste0(format_value(bf))
-          )
-        )
-      )
+format_bf <- function(bf, stars = FALSE, stars_only = FALSE, name = "BF", protect_ratio = FALSE) {
+  bf_orig <- bf
+
+  if (protect_ratio) {
+    is_small <- bf < 1
+    bf[is_small] <- 1 / bf[is_small]
+  } else {
+    is_small <- logical(length(bf))
+  }
+
+  digits <- ifelse(bf < 1, 3, 2)
+
+  ## Very big/small values
+  text <-
+    ifelse(bf_orig > 1000, "> 1000",
+           ifelse(bf_orig < 1 / 1000, ifelse(is_small, "< 1/1000", "< 0.001"),
+                  paste0("= ",
+                         ifelse(is_small, "1/", ""),
+                         format_value(bf, digits = digits))
+           )
     )
-  )
+
+  ## Add stars
+  text <- ifelse(bf_orig > 30, paste0(text, "***"),
+                 ifelse(bf_orig > 10, paste0(text, "**"),
+                        ifelse(bf_orig > 3, paste0(text, "*"), text)
+                        )
+                 )
 
   .add_prefix_and_remove_stars(text, stars, stars_only, name)
 }
