30dd011efab470e0228e1d864adbfd6129fa27d7
Daniel <mail@danielluedecke.de>
2020-10-19 12:33:34 +0200

    close #235

--- R/standardize_names.R
+++ R/standardize_names.R
@@ -6,6 +6,7 @@
 #'
 #' @param data A data frame. In particular, objects from \emph{easystats} package functions like \code{\link[parameters:model_parameters]{model_parameters()}} or \code{\link[effectsize:effectsize]{effectsize()}} are accepted.
 #' @param style Standardization can either be based on the naming conventions from the easystats project, or on \pkg{broom}'s naming scheme.
+#' @param ignore_estimate Logical, if \code{TRUE}, column names like \code{"mean"} or \code{"median"} will \emph{not} be converted to \code{"Coefficient"} resp. \code{"estimate"}.
 #' @param ... Currently not used.
 #'
 #' @return A data frame, with standardized column names.
@@ -44,11 +45,12 @@ standardize_names.default <- function(data, ...) {
 }
 
 
+
 #' @rdname standardize_names
 #' @export
-standardize_names.parameters_model <- function(data, style = c("easystats", "broom"), ...) {
+standardize_names.parameters_model <- function(data, style = c("easystats", "broom"), ignore_estimate = FALSE, ...) {
   style <- match.arg(style)
-  .standardize_names(data, style, ...)
+  .standardize_names(data, style, ignore_estimate = ignore_estimate, ...)
 }
 
 #' @export
@@ -57,15 +59,26 @@ standardize_names.effectsize_table <- standardize_names.parameters_model
 #' @export
 standardize_names.data.frame <- standardize_names.parameters_model
 
+#' @export
+standardize_names.parameters_distribution <- function(data, style = c("easystats", "broom"), ...) {
+  style <- match.arg(style)
+  .standardize_names(data, style, ignore_estimate = TRUE,...)
+}
 
 
 
-.standardize_names <- function(data, style, ...) {
+
+# helper -----
+
+
+.standardize_names <- function(data, style, ignore_estimate = FALSE, ...) {
   cn <- colnames(data)
 
   if (style == "easystats") {
-    cn[cn %in% c("t", "z", "F", "chisq", "Chisq", "chi-sq", "t / F", "z / Chisq")] <- "Statistic"
-    cn[cn %in% c("Median", "Mean", "MAP", "rho", "r", "tau", "Difference")] <- "Coefficient"
+    cn[cn %in% c("t", "z", "F", "Chi2", "chisq", "Chisq", "chi-sq", "t / F", "z / Chisq", "z / Chi2")] <- "Statistic"
+    if (!isTRUE(ignore_estimate)) {
+      cn[cn %in% c("Median", "Mean", "MAP", "rho", "r", "tau", "Difference")] <- "Coefficient"
+    }
     cn[cn %in% c("df_residual", "df_error")] <- "df"
   } else {
     # easy replacements
@@ -85,10 +98,12 @@ standardize_names.data.frame <- standardize_names.parameters_model
     cn[cn == "Sum_Squares"] <- "sumsq"
     cn[cn == "Mean_Square"] <- "meansq"
     # name of coefficient column for (Bayesian) models
-    cn[cn %in% c("Coefficient", "Std_Coefficient", "Median", "Mean", "MAP")] <- "estimate"
+    if (!isTRUE(ignore_estimate)) {
+      cn[cn %in% c("Coefficient", "Std_Coefficient", "Median", "Mean", "MAP")] <- "estimate"
+    }
     # name of coefficient column htest
     cn[cn %in% c("rho", "r", "tau", "Difference")] <- "estimate"
-    cn[cn %in% c("S", "t", "z", "F", "chisq", "chi-sq", "Chisq", "t / F", "z / Chisq")] <- "statistic"
+    cn[cn %in% c("S", "t", "z", "F", "Chi2", "chisq", "chi-sq", "Chisq", "t / F", "z / Chisq", "z / Chi2")] <- "statistic"
     # fancy regex replacements
     cn <- gsub("^CI_low", "conf.low", cn)
     cn <- gsub("^CI_high", "conf.high", cn)
