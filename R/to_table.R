f40ed86a990d2bf81cdae3bea00f8d9ba681a3f7
Daniel <mail@danielluedecke.de>
2020-11-17 21:08:09 +0100

    minor

--- R/to_table.R
+++ R/to_table.R
@@ -1,14 +1,14 @@
-#' Generic export of data frames into tables
+#' @title Generic export of data frames into tables
+#' @name to_table
+#'
+#' @description This is a generic function to export data frames into various
+#' table formats (like plain text, markdown, ...). See the documentation
+#' for the specific objects' classes.
 #'
 #' @param x A data frame.
 #' @param ... Arguments passed to other methods.
 #'
 #' @return A data frame.
-#'
-#' @details This is a generic function to export data frames into various
-#' table formats (like plain text, markdown, ...). See the documentation
-#' for the specific objects' classes.
-#'
 #' @export
 to_table <- function(x, ...) {
   UseMethod("to_table")
