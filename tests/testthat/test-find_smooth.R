4cc992d6593312d023a7c9e5f015ce16bb1f6f16
DominiqueMakowski <dom.mak19@gmail.com>
2020-12-08 21:41:06 +0800

    add tests for find_smooth for all GAM-fitting packages that I'm aware of
    @strengejacke I wonder whether we should return the smooth term "s(smooth_var)" or the smooth *variable* "smooth_var". It seems like for find_random we return the variable rather than the "term" (something like 1|random)

--- tests/testthat/test-find_smooth.R
+++ tests/testthat/test-find_smooth.R
@@ -1,13 +1,33 @@
-if (require("testthat") && require("insight") && require("mgcv")) {
+if (require("testthat") && require("insight") && require("mgcv") && require("gamm4") && require("rstanarm")) {
   set.seed(2) ## simulate some data...
-  dat <- gamSim(1, n = 400, dist = "normal", scale = 2)
+  dat <- mgcv::gamSim(1, n = 400, dist = "normal", scale = 2)
 
-  bt <- gam(y ~ te(x0, x1, k = 7) + s(x2) + s(x3),
+  bt <- mgcv::gam(y ~ te(x0, x1, k = 7) + s(x2) + s(x3),
             data = dat,
             method = "REML")
 
-  test_that("find_smooth", {
+  test_that("find_smooth - gam", {
     expect_equal(find_smooth(bt), list(conditional = c("te(x0, x1, k = 7)", "s(x2)", "s(x3)")))
     expect_equal(find_smooth(bt, flatten = TRUE), c("te(x0, x1, k = 7)", "s(x2)", "s(x3)"))
   })
+
+  test_that("find_smooth - mgcv::gamm", {
+    model <- mgcv::gamm(Petal.Length ~ Petal.Width + s(Sepal.Length), random = list(Species = ~1), data = iris)
+    expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
+  })
+
+  test_that("find_smooth - gamm4", {
+    model <- gamm4::gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length), random=~(1|Species), data = iris)
+    expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
+  })
+
+  test_that("find_smooth - gamm4", {
+    model <- rstanarm::stan_gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length), random=~(1|Species), data = iris, iter=100, chains=1, refresh=0)
+    expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
+  })
+
+  # test_that("find_smooth - brms", {
+  #   model <- brms::brm(Petal.Length ~ Petal.Width + s(Sepal.Length) + (1|Species), data = iris, iter=100, chains=1, refresh=0)
+  #   expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
+  # })
 }
