830db5da81a5c2afae14f6d3ee90b99de7acc4a5
Daniel <mail@danielluedecke.de>
2020-12-09 13:23:39 +0100

    no warning

--- tests/testthat/test-find_smooth.R
+++ tests/testthat/test-find_smooth.R
@@ -26,12 +26,20 @@ if (require("testthat") && require("insight") && require("mgcv") && require("gam
   })
 
   test_that("find_smooth - gamm4", {
-    model <- gamm4::gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length), random=~(1|Species), data = iris)
+    model <- gamm4::gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length), random = ~(1|Species), data = iris)
     expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
   })
 
   test_that("find_smooth - stan_gamm4", {
-    model <- rstanarm::stan_gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length), random=~(1|Species), data = iris, iter=100, chains=1, refresh=0)
+    model <- suppressWarnings(
+      rstanarm::stan_gamm4(
+        Petal.Length ~ Petal.Width + s(Sepal.Length),
+        random =  ~ (1 | Species),
+        data = iris,
+        iter = 100,
+        chains = 1,
+        refresh = 0)
+    )
     expect_equal(find_smooth(model, flatten = TRUE), "s(Sepal.Length)")
   })
 
