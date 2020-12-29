17d8c3b210419882fe7824d84c2e0d6b845e6d74
Daniel <mail@danielluedecke.de>
2020-12-29 11:52:22 +0100

    minor fixes

--- R/get_predicted.R
+++ R/get_predicted.R
@@ -6,14 +6,14 @@
 #'
 #' @examples
 #' x <- lm(Sepal.Length ~ Petal.Width * Species, data=iris)
-#'
 #' get_predicted(x)
 #' @export
 get_predicted <- function(x, ...){
   UseMethod("get_predicted")
 }
 
+#' @importFrom stats fitted
 #' @export
-get_predicted.default <- function(x, ...){
+get_predicted.default <- function(x, ...) {
   stats::fitted(x)
 }
--- R/loglikelihood.R
+++ R/loglikelihood.R
@@ -3,15 +3,17 @@
 #' A robust function to compute the log-likelihood of a model.
 #'
 #' @param estimator Corresponds to the different estimators for the standard deviation of the errors. If \code{estimator="OLS"} (default), it returns the unbiased OLS estimator (scaling by the degrees of freedom, i.e., \code{n−k ()}). If \code{estimator="ML"}, the scaling is done by n (the biased ML estimator), which is then equivalent to using \code{stats::logLik()}. In moderately large samples, the differences should be negligible, but it is possible that OLS would perform slightly better in small samples with Gaussian errors.
+#' @param summarize Currently not used.
 #' @inheritParams get_residuals
 #'
 #' @examples
 #' x <- lm(Sepal.Length ~ Petal.Width + Species, data=iris)
 #'
-#' loglikelihood(x, estimator="OLS")
-#' loglikelihood(x, estimator="ML")  # Equivalent to stats::logLik(x)
+#' loglikelihood(x, estimator = "OLS")
+#' loglikelihood(x, estimator = "ML")  # Equivalent to stats::logLik(x)
+#' @importFrom stats df.residual dnorm
 #' @export
-loglikelihood <- function(x, estimator="OLS", summarize=TRUE, ...) {
+loglikelihood <- function(x, estimator = "OLS", summarize = TRUE, ...) {
 
   if (tolower(estimator) == "ols") {
     # TODO: Replace df.residual by a more robust function
@@ -27,7 +29,7 @@ loglikelihood <- function(x, estimator="OLS", summarize=TRUE, ...) {
   attr(out, "per_obs") <- ll  # This is useful for some models comparison tests
   attr(out, "nall") <- attr(out, "nobs") <- n_obs(x)
   # Not sure about the following, this is only to match the printed output of stats::logLik()
-  attr(out, "df") <- length(find_parameters(x, component="conditional", flatten=TRUE)) + 1
+  attr(out, "df") <- length(find_parameters(x, effects = "fixed", component = "conditional", flatten = TRUE)) + 1
 
   class(out) <- c("logLik", class(x))  # The class returned by stats::logLik(x)
   out
