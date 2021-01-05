7a2fd5b9040873f274b50adb4b26d5aea46d07da
DominiqueMakowski <dom.mak19@gmail.com>
2021-01-05 08:37:25 +0800

    always new line

--- tests/testthat/test-get_loglikelihood.R
+++ tests/testthat/test-get_loglikelihood.R
@@ -4,20 +4,23 @@ if (require("testthat") && require("insight") && require("nonnest2")) {
 
   test_that("get_loglikelihood - lm", {
     x <- lm(Sepal.Length ~ Petal.Width + Species, data=iris)
-    ll <- loglikelihood(x, estimator = "ML"); ll2 <- stats::logLik(x)
+    ll <- loglikelihood(x, estimator = "ML")
+    ll2 <- stats::logLik(x)
     testthat::expect_equal(as.numeric(ll), as.numeric(ll2))
     testthat::expect_equal(attributes(ll)$df, attributes(ll2)$df)
     testthat::expect_equal(sum(attributes(ll)$per_obs - nonnest2::llcont(x)), 0)
 
     # With weights
     x <- lm(Sepal.Length ~ Petal.Width + Species, data=iris, weights=Petal.Length)
-    ll <- loglikelihood(x, estimator = "ML"); ll2 <- stats::logLik(x)
+    ll <- loglikelihood(x, estimator = "ML")
+    ll2 <- stats::logLik(x)
     testthat::expect_equal(as.numeric(ll), as.numeric(ll2))
   })
 
   test_that("get_loglikelihood - glm", {
     x <- glm(vs ~ mpg * disp, data=mtcars, family="binomial")
-    ll <- loglikelihood(x, estimator = "ML"); ll2 <- stats::logLik(x)
+    ll <- loglikelihood(x, estimator = "ML")
+    ll2 <- stats::logLik(x)
     testthat::expect_equal(as.numeric(ll), as.numeric(ll2))
     testthat::expect_equal(attributes(ll)$df, attributes(ll2)$df)
     testthat::expect_equal(sum(attributes(ll)$per_obs - nonnest2::llcont(x)), 0)
@@ -26,12 +29,14 @@ if (require("testthat") && require("insight") && require("nonnest2")) {
   test_that("get_loglikelihood - (g)lmer", {
     if(require("lme4")){
       x <- lme4::lmer(Sepal.Length ~ Sepal.Width + (1|Species), data=iris)
-      ll <- loglikelihood(x, estimator = "ML"); ll2 <- stats::logLik(x)
+      ll <- loglikelihood(x, estimator = "ML")
+      ll2 <- stats::logLik(x)
       testthat::expect_equal(as.numeric(ll), as.numeric(ll2))
       testthat::expect_equal(attributes(ll)$df, attributes(ll2)$df)
 
       x <- lme4::glmer(vs ~ mpg + (1|cyl), data=mtcars, family="binomial")
-      ll <- loglikelihood(x, estimator = "ML"); ll2 <- stats::logLik(x)
+      ll <- loglikelihood(x, estimator = "ML")
+      ll2 <- stats::logLik(x)
       testthat::expect_equal(as.numeric(ll), as.numeric(ll2))
       testthat::expect_equal(attributes(ll)$df, attributes(ll2)$df)
     }
