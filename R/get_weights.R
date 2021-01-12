3692a992295cbd278fba03a3c2a68859e691e1fb
Daniel <mail@danielluedecke.de>
2021-01-12 22:46:56 +0100

    pass dots to stats::weights()

--- R/get_weights.R
+++ R/get_weights.R
@@ -42,7 +42,7 @@ get_weights.default <- function(x, na_rm = FALSE, null_as_ones = FALSE, ...) {
   w <- NULL
   tryCatch(
     {
-      w <- stats::weights(x)
+      w <- stats::weights(x, ...)
     },
     error = function(e) {
       NULL
