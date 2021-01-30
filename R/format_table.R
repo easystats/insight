59ade9741ace9ea100e32a1b6773a51d1a4af84f
mattansb <35330040+mattansb@users.noreply.github.com>
2021-01-30 16:19:44 +0200

    close #288

--- R/format_table.R
+++ R/format_table.R
@@ -142,7 +142,7 @@ format_table <- function(x, pretty_names = TRUE, stars = FALSE, digits = 2, ci_w
 
   # restore attributes
   if (isTRUE(preserve_attributes)) {
-    attributes(x) <- utils::modifyList(att, attributes(x))
+    mostattributes(x) <- att
   }
 
   x
