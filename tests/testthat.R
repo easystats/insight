8036ab05d0032ee62e3061db58d92524038967cb
Daniel <mail@danielluedecke.de>
2021-02-17 19:37:31 +0100

    skip all tests on mac os

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,4 +7,15 @@ if (length(strsplit(packageDescription("insight")$Version, "\\.")[[1]]) > 3) {
   Sys.setenv("RunAllinsightTests" = "no")
 }
 
-test_check("insight")
+osx <- tryCatch({
+  si <- Sys.info()
+  if (!is.null(si["sysname"])) {
+    si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+  } else {
+    FALSE
+  }
+})
+
+if (!osx) {
+  test_check("insight")
+}
