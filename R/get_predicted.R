e8a4bf6c7acd0e8dd16abd085a3da04798ca0e1a
DominiqueMakowski <dom.mak19@gmail.com>
2021-02-23 11:24:47 +0800

    minor docs

--- R/get_predicted.R
+++ R/get_predicted.R
@@ -5,7 +5,7 @@
 #' \code{as.data.frame()} (see examples below).
 #'
 #' @param ... Not used.
-#' @param ci_type Can be \code{"prediction"} or \code{"confidence"}. Prediction intervals show the range that likely contains the value of a new observation (in what range it would fall), whereas confidence intervals reflect the uncertainty around the estimated parameters (and gives the range of the link; for instance of the regression line in a linear regressions). Prediction intervals account for both the uncertainty in the model's parameters, plus the random variation of the individual values. Thus, prediction intervals are always wider than confidence intervals. Moreover, prediction intervals will not necessarily become narrower as the sample size increases (as they do not reflect only the quality of the fit). This applies mostly for "simple" linear models (like \code{lm}), as for other models (e.g., \code{glm}), prediction intervals are somewhat useless (for instance, for a binomial model for which the dependent variable is a vector of 1s and 0s, the prediction interval is... \code{[0, 1]}).
+#' @param ci_type Can be \code{"prediction"} or \code{"confidence"}. Prediction intervals show the range that likely contains the value of a new observation (in what range it would fall), whereas confidence intervals reflect the uncertainty around the estimated parameters (and gives the range of the link; for instance of the regression line in a linear regressions). Prediction intervals account for both the uncertainty in the model's parameters, plus the random variation of the individual values. Thus, prediction intervals are always wider than confidence intervals. Moreover, prediction intervals will not necessarily become narrower as the sample size increases (as they do not reflect only the quality of the fit). This applies mostly for "simple" linear models (like \code{lm}), as for other models (e.g., \code{glm}), prediction intervals are somewhat useless (for instance, for a binomial model for which the dependent variable is a vector of 1s and 0s, the prediction interval is... \code{[0, 1]}). Importantly for Bayesian models, it is this argument that modulates the type of prediction - i.e., link (using  \code{\link[rstanarm:predictive_interval]{predictive_interval()}}) or predictive (using \code{\link[rstanarm:posterior_predict]{posterior_predict}}).
 #' @param ci The interval level (default \code{0.95}, i.e., 95\% CI).
 #' @param transform Either \code{"response"} (default) or \code{"link"}. If \code{"link"}, no transformation is applied and the values are on the scale of the linear predictors. If \code{"response"}, the output is on the scale of the response variable. Thus for a default binomial model, \code{"response"} gives the predicted probabilities, and \code{"link"} makes predictions of log-odds (probabilities on logit scale).
 #' @param include_random If \code{TRUE} (default), include all random effects in the prediction. If \code{FALSE}, don't take them into account. Can also be a formula to specify which random effects to condition on when predicting (passed to the \code{re.form} argument). If \code{include_random = TRUE} and \code{newdata} is provided, make sure to include the random effect variables in \code{newdata} as well.
@@ -24,6 +24,7 @@
 #' @inheritParams get_residuals
 #' @inheritParams stats::predict.lm
 #'
+#'
 #' @return The fitted values (i.e. predictions for the response).
 #'
 #' @note Currently, this function just calls \code{stats::fitted()}, but will
@@ -39,6 +40,13 @@
 #' # Get CI
 #' attributes(predicted)$CI_low # Or CI_high
 #' as.data.frame(predicted)
+#'
+#' \donttest{
+#' # Bayesian models
+#' if (require("rstanarm") && require("bayestestR")) {
+#'   model <- stan_glm(mpg ~ am, data = mtcars, refresh = 0)
+#'   describe_posterior(insight::get_predicted(model, ci_type = "prediction"))
+#' }}
 #' @export
 get_predicted <- function(x, ...) {
   UseMethod("get_predicted")
