8e1f934387a31f8efabd0c714e7709e34633211d
Daniel <mail@danielluedecke.de>
2021-03-12 08:07:01 +0100

    add color_text

--- R/print_color.R
+++ R/print_color.R
@@ -11,7 +11,8 @@
 #'   with \code{"bold"} or \code{"italic"}.
 #'
 #' @details This function prints \code{text} directly to the console using
-#'   \code{cat()}, so no string is returned.
+#'   \code{cat()}, so no string is returned. \code{color_text()}, however,
+#'   returns only the formatted string, without using \code{cat()}.
 #'
 #' @return Nothing.
 #'
@@ -26,4 +27,17 @@ print_color <- function(text, color) {
 #' @export
 print_colour <- function(text, colour) {
   print_color(color = colour, text = text)
+}
+
+
+#' @rdname print_color
+#' @export
+color_text <- function(text, color) {
+  .colour(colour = color, x = text)
+}
+
+#' @rdname print_color
+#' @export
+colour_text <- function(text, colour) {
+  .colour(colour = colour, x = text)
 }
