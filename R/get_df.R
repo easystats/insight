1a01081f19799b7b3def7a416cff029368a5c280
Daniel <mail@danielluedecke.de>
2021-04-02 20:42:25 +0200

    add more df

--- R/get_df.R
+++ R/get_df.R
@@ -78,7 +78,26 @@ get_df.summary.lm <- function(x, type = "residual", ...) {
 
 #' @export
 get_df.emmGrid <- function(x, ...) {
+  if (!is.null(x@misc$is_boot) && x@misc$is_boot) {
+    return(.boot_em_df(x))
+  }
   unique(summary(x)$df)
+}
+
+
+#' @export
+get_df.emmGrid <- function(x, ...) {
+  if (!is.null(x[[1]]@misc$is_boot) && x[[1]]@misc$is_boot) {
+    return(.boot_em_df(x))
+  }
+  s <- summary(x)
+  unname(unlist(lapply(s, function(i) {
+    if (is.null(i$df)) {
+      rep(Inf, nrow(i))
+    } else {
+      i$df
+    }
+  })))
 }
 
 
@@ -179,6 +198,71 @@ get_df.betaor <- get_df.logitor
 get_df.betamfx <- get_df.logitor
 
 
+#' @export
+get_df.merModList <- function(x, type = "residual", ...) {
+  type <- match.arg(tolower(type), choices = c("residual", "model"))
+  if (type == "model") {
+    .model_df(x)
+  } else {
+    s <- suppressWarnings(summary(x))
+    s$fe$df
+  }
+}
+
+
+#' @export
+get_df.mira <- function(x, type = "residual", ...) {
+  if (!requireNamespace("mice", quietly = TRUE)) {
+    stop("Package 'mice' needed for this function to work. Please install it.")
+  }
+  type <- match.arg(tolower(type), choices = c("residual", "model"))
+  get_df(mice::pool(x), type, ...)
+}
+
+
+#' @export
+get_df.mipo <- function(x, type = "residual", ...) {
+  type <- match.arg(tolower(type), choices = c("residual", "model"))
+  if (type == "model") {
+    .model_df(x)
+  } else {
+    as.vector(summary(x)$df)
+  }
+}
+
+
+#' @export
+get_df.vgam <- function(x, type = "residual", ...) {
+  type <- match.arg(tolower(type), choices = c("residual", "model"))
+  if (type == "model") {
+    .model_df(x)
+  } else {
+    params <- get_parameters(x)
+    out <- setNames(rep(NA, nrow(params)), params$Parameter)
+    out[names(x@nl.df)] <- x@nl.df
+    out
+  }
+}
+
+
+#' @export
+get_df.rqs <- function(x, type = "residual", ...) {
+  type <- match.arg(tolower(type), choices = c("residual", "model"))
+  if (type == "model") {
+    .model_df(x)
+  } else {
+    tryCatch(
+      {
+        s <- suppressWarnings(summary(x, covariance = TRUE))
+        cs <- lapply(s, function(i) i$rdf)
+        unique(unlist(cs))
+      },
+      error = function(e) {
+        NULL
+      }
+    )
+  }
+}
 
 
 
@@ -269,4 +353,10 @@ get_df.betamfx <- get_df.logitor
   }
 
   dof
+}
+
+
+.boot_em_df <- function(model) {
+  est <- get_parameters(model, summary = FALSE)
+  rep(NA, ncol(est))
 }
