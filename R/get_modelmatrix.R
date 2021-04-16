a2fc5d2e1ebadbfa4eab15ec49b21d72332d9685
Daniel <mail@danielluedecke.de>
2021-04-16 13:22:21 +0200

    docs

--- R/get_modelmatrix.R
+++ R/get_modelmatrix.R
@@ -2,16 +2,14 @@
 #'
 #' Creates a design matrix from the description. Any character variables are coerced to factors.
 #'
-#'
 #' @param x An object.
-#' @param ... Not used.
+#' @param ... Passed down to other methods (mainly \code{model.matrix()}).
 #'
 #' @examples
 #' data(mtcars)
 #'
 #' model <- lm(am ~ vs, data = mtcars)
 #' get_modelmatrix(model)
-#'
 #' @export
 get_modelmatrix <- function(x, ...) {
   UseMethod("get_modelmatrix")
