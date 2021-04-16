5a75a80e5b5eaa22e85d98766797091433d0c89a
Dominique Makowski <dom.mak19@gmail.com>
2021-04-16 17:46:39 +0800

    initialize get_modelmatrix

---
+++ R/get_modelmatrix.R
@@ -0,0 +1,23 @@
+#' Model Matrix
+#'
+#' Creates a design matrix from the description. Any character variables are coerced to factors.
+#'
+#'
+#' @param x An object.
+#'
+#' @examples
+#' data(mtcars)
+#'
+#' model <- lm(am ~ vs, data = mtcars)
+#' get_modelmatrix(model)
+#'
+#' @export
+get_modelmatrix <- function(x, ...) {
+  UseMethod("get_modelmatrix")
+}
+
+
+#' @export
+get_modelmatrix.default <- function(x, ...) {
+  model.matrix(x, ...)
+}
