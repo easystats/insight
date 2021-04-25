d52155599d3f84b36fa2abcab499639aa3fa8e24
Daniel <mail@danielluedecke.de>
2021-04-25 21:02:41 +0200

    https://github.com/easystats/easystats/issues/116

--- R/format_message.R
+++ R/format_message.R
@@ -6,23 +6,49 @@
 #'   can be accessed. By default, new lines are indented by two whitespace.
 #'
 #' @param string A string.
+#' @param ... Further strings that will be concatenated as indented new lines.
 #' @param line_length Numeric, the maximum length of a line.
 #'
 #' @return A formatted string.
 #' @examples
-#' format_message("Much too long string for just one line, I guess!", 15)
+#' msg <- format_message("Much too long string for just one line, I guess!",
+#'                       line_length = 15)
+#' message(msg)
+#'
+#' msg <- format_message("Much too long string for just one line, I guess!",
+#'                       "First new line",
+#'                       "Second new line",
+#'                       "(both indented)",
+#'                       line_length = 30)
+#' message(msg)
 #' @export
-format_message <- function(string, line_length = options()$width) {
+format_message <- function(string, ..., line_length = options()$width) {
   if (is.null(line_length) || is.infinite(line_length) || line_length < 1) {
     line_length <- 70
   }
 
-  linesep <- "\\1\n  "
-  lsub <- 0
+  string <- .wrap_message_line(string, line_length)
+  further_lines <- list(...)
 
+  if (length(further_lines)) {
+    further_lines <- lapply(further_lines, function(i) {
+      .wrap_message_line(string = i, line_length = line_length, indention = "  ")
+    })
+    string <- paste0(c(string, unlist(further_lines)), collapse = "\n")
+  }
+
+  string
+}
+
+
+
+.wrap_message_line <- function(string, line_length, indention = NULL) {
+  line_separator <- "\\1\n  "
+  lsub <- 0
   pattern <- paste("(.{1,", line_length, "})(\\s|$)", sep = "")
+
   if (line_length > 0 && nchar(string) > line_length) {
-    string <- gsub(pattern, linesep, string)
+    string <- gsub(pattern, line_separator, string)
     l <- nchar(string)
     lc <- substr(string, l - lsub, l)
     if (lc == "\n") {
@@ -33,6 +59,10 @@ format_message <- function(string, line_length = options()$width) {
   # remove trailing newline
   if (grepl("\\n  $", string)) {
     string <- gsub("\\n  $", "", string)
+  }
+
+  if (!is.null(indention)) {
+    string <- paste0(indention, string)
   }
 
   string
