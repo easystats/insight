2423d8a3a660ed2165be37fff65468f2ea2f46bf
Daniel <mail@danielluedecke.de>
2021-04-25 13:02:02 +0200

    tests only conditionally

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,26 +1,27 @@
-library(testthat)
-library(insight)
+if (require("testthat")) {
+  library(insight)
 
-if (length(strsplit(packageDescription("insight")$Version, "\\.")[[1]]) > 3) {
-  Sys.setenv("RunAllinsightTests" = "yes")
-} else {
-  Sys.setenv("RunAllinsightTests" = "no")
-}
+  if (length(strsplit(packageDescription("insight")$Version, "\\.")[[1]]) > 3) {
+    Sys.setenv("RunAllinsightTests" = "yes")
+  } else {
+    Sys.setenv("RunAllinsightTests" = "no")
+  }
 
-osx <- tryCatch(
-  {
-    si <- Sys.info()
-    if (!is.null(si["sysname"])) {
-      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-    } else {
+  osx <- tryCatch(
+    {
+      si <- Sys.info()
+      if (!is.null(si["sysname"])) {
+        si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+      } else {
+        FALSE
+      }
+    },
+    error = function(e) {
       FALSE
     }
-  },
-  error = function(e) {
-    FALSE
-  }
-)
+  )
 
-if (!osx || (osx && getRversion() >= "4.0.0")) {
-  test_check("insight")
+  if (!osx || (osx && getRversion() >= "4.0.0")) {
+    test_check("insight")
+  }
 }
