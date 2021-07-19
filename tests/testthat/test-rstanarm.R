6a3e40aa419d52a94d8a7d3cd4516b6c145ab59f
Daniel <mail@danielluedecke.de>
2021-07-19 13:54:48 +0200

    disable test for now

--- tests/testthat/test-rstanarm.R
+++ tests/testthat/test-rstanarm.R
@@ -100,16 +100,16 @@ if (.runThisTest) {
       )
     }
 
-    data("Orange", package = "datasets")
-    Orange$circumference <- Orange$circumference / 100
-    Orange$age <- Orange$age / 100
-    m14 <- stan_nlmer(
-      circumference ~ SSlogis(age, Asym, xmid, scal) ~ Asym | Tree,
-      data = Orange,
-      # for speed only
-      chains = 1,
-      iter = 1000
-    )
+    # data("Orange", package = "datasets")
+    # Orange$circumference <- Orange$circumference / 100
+    # Orange$age <- Orange$age / 100
+    # m14 <- stan_nlmer(
+    #   circumference ~ SSlogis(age, Asym, xmid, scal) ~ Asym | Tree,
+    #   data = Orange,
+    #   # for speed only
+    #   chains = 1,
+    #   iter = 1000
+    # )
 
     m15 <- stan_mvmer(
       formula = list(
