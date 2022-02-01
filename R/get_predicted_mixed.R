8fe5a01ae9645efde369e25664ffa212c6674dcb
Daniel <mail@danielluedecke.de>
2022-02-01 18:06:02 +0100

    add get_predicted.MixMod

--- R/get_predicted_mixed.R
+++ R/get_predicted_mixed.R
@@ -171,3 +171,98 @@ get_predicted.glmmTMB <- function(x,
   # 4. step: final preparation
   .get_predicted_out(out$predictions, args = args, ci_data = out$ci_data)
 }
+
+
+
+
+# GLMMadaptive: mixed_model (class MixMod) ------------------------------
+# =======================================================================
+
+#' @export
+get_predicted.MixMod <- function(x,
+                                 data = NULL,
+                                 predict = "expectation",
+                                 ci = 0.95,
+                                 include_random = TRUE,
+                                 iterations = NULL,
+                                 verbose = TRUE,
+                                 ...) {
+  # Sanity checks
+  if (!is.null(predict) && predict %in% c("prediction", "predicted", "classification")) {
+    predict <- "expectation"
+    if (verbose) {
+      warning(
+        format_message(
+          '"prediction" and "classification" are currently not supported by the',
+          '`predict` argument for GLMMadaptive models. Changing to `predict="expectation"`.'
+        ),
+        call. = FALSE
+      )
+    }
+  }
+
+  # Sanitize input
+  args <- .get_predicted_args(
+    x,
+    data = data,
+    predict = predict,
+    ci = ci,
+    include_random = include_random,
+    verbose = verbose,
+    ...
+  )
+
+  # Prediction function
+  predict_function <- function(x, data, ...) {
+    stats::predict(
+      x,
+      newdata = data,
+      type_pred = args$type,
+      type = ifelse(isTRUE(args$include_random), "subject_specific", "mean_subject"),
+      ...
+    )
+  }
+
+  # 1. step: predictions
+  rez <- predict_function(x, data = args$data)
+
+  if (is.null(iterations)) {
+    predictions <- as.numeric(rez)
+  } else {
+    predictions <- .get_predicted_boot(
+      x,
+      data = args$data,
+      predict_function = predict_function,
+      iterations = iterations,
+      verbose = verbose,
+      ...
+    )
+  }
+
+  # "expectation" for zero-inflated? we need a special handling
+  # for predictions and CIs here.
+
+  if (args$scale == "response" && args$info$is_zero_inflated) {
+
+    # 2. and 3. step: confidence intervals and back-transform
+    ci_data <- .simulate_zi_predictions(model = x, newdata = data, predictions = predictions, nsim = iterations, ci = ci)
+    out <- list(predictions = predictions, ci_data = ci_data)
+
+  } else {
+
+    # 2. step: confidence intervals
+    ci_data <- get_predicted_ci(
+      x,
+      predictions,
+      data = args$data[colnames(args$data) != find_response(x)],
+      ci_type = args$ci_type,
+      ...
+    )
+
+    # 3. step: back-transform
+    out <- .get_predicted_transform(x, predictions, args, ci_data)
+  }
+
+  # 4. step: final preparation
+  .get_predicted_out(out$predictions, args = args, ci_data = out$ci_data)
+}
