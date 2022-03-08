e14057535cc3a0755ea89124dabb94fc92ec83c7
Daniel <mail@danielluedecke.de>
2022-03-08 09:50:33 +0100

    compact

--- R/get_predicted_bayesian.R
+++ R/get_predicted_bayesian.R
@@ -48,19 +48,10 @@ get_predicted.stanreg <- function(x,
 
   # The following makes the argument passed to predict "nsamples" or "ndraws",
   # as it got changed in recent brms versions (but we want to preserve compatibility)
-  if ("nsamples" %in%
-      names(formals(getS3method(
-        "posterior_predict",
-        class(x)
-      )))
-  ) {
-    fun_args <- c(
-      fun_args, nsamples = iterations
-    )
+  if ("nsamples" %in% names(formals(utils::getS3method("posterior_predict", class(x))))) {
+    fun_args <- c(fun_args, nsamples = iterations)
   } else {
-    fun_args <- c(
-      fun_args, ndraws = iterations
-    )
+    fun_args <- c(fun_args, ndraws = iterations)
   }
 
   # Fix dots content
