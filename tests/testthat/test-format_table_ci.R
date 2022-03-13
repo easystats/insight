8b3a9908141c590f7f2d58c2d9ff2b632f339937
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-03-13 11:14:30 +0200

    print si tests

--- tests/testthat/test-format_table_ci.R
+++ tests/testthat/test-format_table_ci.R
@@ -21,4 +21,30 @@ if (requiet("testthat") && requiet("insight")) {
     ft <- insight::format_table(d)
     expect_equal(colnames(ft), c("97% CI", "20% CI"))
   })
+
+
+  test_that("format_table with si-level", {
+    d <- data.frame(CI = 0.97, CI_low = 1, CI_high = 3)
+    attr(d, "ci_method") <- "SI"
+    ft <- insight::format_table(d)
+    expect_equal(colnames(ft), "BF = 0.97 SI")
+
+    d$CI <- 0.788
+    ft <- insight::format_table(d)
+    expect_equal(colnames(ft), "BF = 0.788 SI")
+
+    d$CI <- NULL
+    attr(d, "ci") <- 0.9
+    ft <- insight::format_table(d)
+    expect_equal(colnames(ft), "BF = 0.9 SI")
+  })
+
+
+  test_that("format_table with multiple si-levels", {
+    d <- data.frame(CI_low_3 = 1, CI_high_3 = 3,
+                    CI_low_0.2 = 1, CI_high_0.2 = 3)
+    attr(d, "ci_method") <- "SI"
+    ft <- insight::format_table(d)
+    expect_equal(colnames(ft), c("BF = 3 SI", "BF = 0.2 SI"))
+  })
 }
