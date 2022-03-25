ba8a98e9225990b9c75fe9c267057d8ea7a163b6
Daniel <mail@danielluedecke.de>
2022-03-25 15:55:39 +0100

    Update tests/testthat/test-get_predicted.R
    Co-authored-by: Brenton M. Wiernik <bwiernik@users.noreply.github.com>

--- tests/testthat/test-get_predicted.R
+++ tests/testthat/test-get_predicted.R
@@ -457,7 +457,7 @@ test_that("lm: get_predicted vs barebones `predict()`", {
 
 test_that("using both `predict` and `type` raises an informative warning", {
   mod <- glm(am ~ hp + factor(cyl), family = binomial, data = mtcars)
-  expect_warning(get_predicted(mod, predict = "response", type = "response"))
+  expect_message(get_predicted(mod, predict = "response", type = "response"))
 })
 
 
