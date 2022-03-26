748f3c01b38410b0b95c666826ac7ab891cdfb8b
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-03-26 08:36:51 -0400

    get_varcov_sandwich: safer

--- tests/testthat/test-get_varcov.R
+++ tests/testthat/test-get_varcov.R
@@ -1,10 +1,13 @@
 requiet("sandwich")
 requiet("clubSandwich")
 
-test_that("warning in generic get_varcov: unsupported by sandwich", {
+test_that("informative error in get_varcov.default", {
     requiet("lme4")
     mod <- lmer(mpg ~ hp + (1 | cyl), data = mtcars)
-    expect_error(expect_warning(get_varcov(mod, vcov = "HC2"), regexp = "not be supported"))
+    # sandwich: not supported
+    expect_error(get_varcov(mod, vcov = "HC2"), regexp = "not be supported")
+    # clubSandwich: supported
+    expect_error(get_varcov(mod, vcov = "CR0"), NA)
 })
 
 test_that("lm: sandwich", {
@@ -22,7 +25,9 @@ test_that("lm: sandwich", {
 
 test_that("lm: clubSandwich", {
     mod <- lm(mpg ~ hp * wt, data = mtcars)
-    expect_equal(get_varcov(mod, vcov = "CR", vcov_args = list(cluster = mtcars$cyl, type = "CR0")),
+    expect_equal(get_varcov(mod,
+                            vcov = "CR",
+                            vcov_args = list(cluster = mtcars$cyl, type = "CR0")),
                  clubSandwich::vcovCR(mod, cluster = mtcars$cyl, type = "CR0"),
                  tolerance = 1e-5,
                  ignore_attr = TRUE)
