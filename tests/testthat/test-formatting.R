da3b312168794a5d1258243754842f47170a96d3
Daniel <mail@danielluedecke.de>
2022-04-18 22:55:59 +0200

    Update test-formatting.R

--- tests/testthat/test-formatting.R
+++ tests/testthat/test-formatting.R
@@ -14,4 +14,10 @@ if (requiet("testthat") && requiet("insight")) {
     expect_equal(format_p(p, name = NULL, whitespace = FALSE), c(">.999", "0.900", "0.050", "0.010", "0.001", "<.001"))
     expect_equal(format_p(p, name = NULL, whitespace = FALSE, decimal_separator = ","), c(">,999", "0,900", "0,050", "0,010", "0,001", "<,001"))
   })
+
+  test_that("format_message", {
+    msg <- "R is free software and comes with ABSOLUTELY NO WARRANTY. You are welcome to redistribute it under certain conditions. Type 'license()' or 'licence()' for distribution details."
+    expect_equal(format_message(msg), "R is free software and comes with ABSOLUTELY NO WARRANTY. You are welcome to redistribute it\n  under certain conditions. Type 'license()' or 'licence()' for distribution details.")
+    expect_equal(format_message(msg, line_length = 43.35), "R is free software and comes with\n  ABSOLUTELY NO WARRANTY. You are welcome to\n  redistribute it under certain conditions.\n  Type 'license()' or 'licence()' for\n  distribution details.")
+  })
 }
