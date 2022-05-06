d4cf472424c247a53a0367d51b20de7948a3efd7
Daniel <mail@danielluedecke.de>
2022-05-06 22:52:46 +0200

    Update test-null_model.R

--- tests/testthat/test-null_model.R
+++ tests/testthat/test-null_model.R
@@ -30,12 +30,11 @@ if (.runThisTest &&
   x <- runif(N, 0, 10) # Predictor
   off <- rgamma(N, 3, 2) # Offset variable
   yhat <- -1 + x * 0.5 + log(off) # Prediction on log scale
-  dat <- data.frame(y = NA, x, logOff = log(off)) # Storage dataframe
 
-  dat$y <- rpois(N, exp(yhat)) # Poisson process
-  dat$y <- ifelse(rbinom(N, 1, 0.3), 0, dat$y) # Zero-inflation process
+  y <- rpois(N, exp(yhat)) # Poisson process
+  y <- ifelse(rbinom(N, 1, 0.3), 0, y) # Zero-inflation process
 
-  d <<- dat
+  d <<- data.frame(y = y, x, logOff = log(off)) # Storage dataframe
 
   m1 <- glm(y ~ x + offset(logOff), data = d, family = "poisson")
   m2 <- glm(y ~ x, offset = logOff, data = d, family = "poisson")
