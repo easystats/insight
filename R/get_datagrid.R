6cd5290c81cfda5d00e9345c936cbdd7377035b9
Daniel <mail@danielluedecke.de>
2022-08-31 13:30:07 +0200

    need verbose arg

--- R/get_datagrid.R
+++ R/get_datagrid.R
@@ -719,6 +719,7 @@ get_datagrid.default <- function(x,
                                  include_random = FALSE,
                                  include_response = FALSE,
                                  data = NULL,
+                                 verbose = TRUE,
                                  ...) {
   # sanity check
   if (!is_model(x)) {
@@ -735,7 +736,7 @@ get_datagrid.default <- function(x,
   # usually require the response to be included in the model, else `get_modelmatrix()`
   # fails, which is required to compute SE/CI for `get_predicted()`
   minfo <- model_info(x)
-  if (minfo$is_binomial && minfo$is_logit && is.factor(response) && !include_response && verbose) {
+  if (minfo$is_binomial && minfo$is_logit && is.factor(data[[response]]) && !include_response && verbose) {
     waning(format_message(
       "Logistic regression model has a categorical response variable. You may need to set `include_response=TRUE` to make it work for predictions."
     ), call. = FALSE)
