4acfdf375c8b690c00c3604d78e4f33b125c4681
Daniel <mail@danielluedecke.de>
2022-08-31 13:28:42 +0200

    make sure warning is thrown

--- tests/testthat/test-get_datagrid.R
+++ tests/testthat/test-get_datagrid.R
@@ -195,7 +195,7 @@ if (requiet("testthat") && requiet("insight") && requiet("gamm4") && requiet("gl
 
     grid <- insight::get_datagrid(
       model, at = "k618", range = "grid", preserve_range = FALSE,
-      verbose = FALSE, include_response = TRUE
+      verbose = TRUE, include_response = TRUE
     )
     expect_equal(
       sapply(grid, class),
