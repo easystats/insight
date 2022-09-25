dde16dbe6ba0f09d266f3cf8ee60dddfab392809
Daniel <mail@danielluedecke.de>
2022-09-26 00:05:32 +0200

    fix test

--- tests/testthat/test-polr.R
+++ tests/testthat/test-polr.R
@@ -35,25 +35,23 @@ if (requiet("testthat") &&
     )
   })
 
-  if (requiet("parameters")) {
-    test_that("get_df", {
-      expect_equal(
-        get_df(m1, type = "residual"),
-        df.residual(m1),
-        ignore_attr = TRUE
-      )
-      expect_equal(
-        get_df(m1, type = "normal"),
-        Inf,
-        ignore_attr = TRUE
-      )
-      expect_equal(
-        get_df(m1, type = "wald"),
-        parameters::degrees_of_freedom(m1, method = "wald"),
-        ignore_attr = TRUE
-      )
-    })
-  }
+  test_that("get_df", {
+    expect_equal(
+      get_df(m1, type = "residual"),
+      df.residual(m1),
+      ignore_attr = TRUE
+    )
+    expect_equal(
+      get_df(m1, type = "normal"),
+      Inf,
+      ignore_attr = TRUE
+    )
+    expect_equal(
+      get_df(m1, type = "wald"),
+      df.residual(m1), # model has t-statistic
+      ignore_attr = TRUE
+    )
+  })
 
   test_that("find_formula", {
     expect_length(find_formula(m1), 1)
