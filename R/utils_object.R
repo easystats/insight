4c1621ffdbcebf4743e9493ee394b00a80e99914
etiennebacher <etienne.bacher@protonmail.com>
2022-09-29 15:11:36 +0200

    improve performance of object_has_rownames()

--- R/utils_object.R
+++ R/utils_object.R
@@ -84,5 +84,5 @@ object_has_rownames <- function(x) {
     stop("Only dataframe objects are allowed.", call. = FALSE)
   }
 
-  !identical(as.character(seq_len(nrow(x))), rownames(x))
+  !all(is.numeric(attributes(x)$row.names))
 }
