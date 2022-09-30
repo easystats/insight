410b019477593ab368c6dacfd072f45747e4069e
Daniel <mail@danielluedecke.de>
2022-09-30 14:00:10 +0200

    https://github.com/easystats/easystats/issues/314

---
+++ R/insight-package.R
@@ -0,0 +1,26 @@
+#' @title insight: A Unified Interface to Access Information from Model Objects in R.
+#'
+#' @description
+#'
+#' When fitting any statistical model, there are many useful pieces of information
+#' that are simultaneously calculated and stored beyond coefficient estimates
+#' and general model fit statistics. Although there exist some generic functions
+#' to obtain model information and data, many package-specific modelling functions
+#' do not provide such methods to allow users to access such valuable information.
+#'
+#' **insight** is an R-package that fills this important gap by providing a
+#' suite of functions to support almost any model (see a list of the many models
+#' supported below in the **List of Supported Packages and Models** section).
+#' The goal of **insight**, then, is to provide tools to provide *easy*,
+#' *intuitive*, and *consistent* access to information contained in model
+#' objects. These tools aid applied research in virtually any field who fit,
+#' diagnose, and present statistical models by streamlining access to every
+#' aspect of many model objects via consistent syntax and output.
+#'
+#' References: Lüdecke et al. (2019) <doi:10.21105/joss.01412>.
+#'
+#' @docType package
+#' @aliases insight insight-package
+#' @name insight-package
+#' @keywords internal
+"_PACKAGE"
