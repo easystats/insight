f61da2e33d28457155e2d8ffdd19bbf7728615be
Daniel <mail@danielluedecke.de>
2022-10-03 18:54:21 +0200

    update test

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -35,15 +35,19 @@ test_that("get_data include weights, even if ones", {
 
   #Model with nonuniform weights
   fn <- lm(y ~ x, weights = wn)
-  expect_equal(colnames(insight::get_data(fn)), c("y", "x", "(weights)", "wn"))
+  expect_equal(colnames(get_data(fn)), c("y", "x", "(weights)", "wn"))
 
   #Model with weights equal to 1
   f1 <- lm(y ~ x, weights = w1)
-  expect_equal(colnames(insight::get_data(f1)), c("y", "x", "(weights)", "w1"))
+  expect_equal(colnames(get_data(f1)), c("y", "x", "(weights)", "w1"))
 
   #Model with no weights
   f0 <- lm(y ~ x)
-  expect_equal(colnames(insight::get_data(f0)), c("y", "x"))
+  expect_equal(colnames(get_data(f0)), c("y", "x"))
+
+  # check get_weights still works
+  expect_null(get_weights(f0))
+  expect_equal(get_weights(f0, null_as_ones = TRUE), w1)
 })
 
 
