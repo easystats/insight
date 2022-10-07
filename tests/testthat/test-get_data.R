987371d349eee3b5ce2dc223e4d12c5a172e8318
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-10-07 15:24:33 -0400

    fix bad merge

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -35,8 +35,6 @@ test_that("get_data include weights, even if ones", {
 
   #Model with nonuniform weights
   fn <- lm(y ~ x, weights = wn)
-<<<<<<< HEAD
-<<<<<<< HEAD
   expect_equal(colnames(get_data(fn)), c("y", "x", "(weights)", "wn"))
 
   #Model with weights equal to 1
@@ -50,11 +48,7 @@ test_that("get_data include weights, even if ones", {
   # check get_weights still works
   expect_null(get_weights(f0))
   expect_equal(get_weights(f0, null_as_ones = TRUE), w1)
-=======
-  expect_equal(colnames(insight::get_data(fn)), c("y", "x", "(weights)", "wn"))
-=======
   expect_equal(colnames(get_data(fn)), c("y", "x", "(weights)", "wn"))
->>>>>>> 93446a48 (update test)
 
   #Model with weights equal to 1
   f1 <- lm(y ~ x, weights = w1)
@@ -62,16 +56,11 @@ test_that("get_data include weights, even if ones", {
 
   #Model with no weights
   f0 <- lm(y ~ x)
-<<<<<<< HEAD
-  expect_equal(colnames(insight::get_data(f0)), c("y", "x"))
->>>>>>> 2e239400 (add tests)
-=======
   expect_equal(colnames(get_data(f0)), c("y", "x"))
 
   # check get_weights still works
   expect_null(get_weights(f0))
   expect_equal(get_weights(f0, null_as_ones = TRUE), w1)
->>>>>>> 93446a48 (update test)
 })
 
 
