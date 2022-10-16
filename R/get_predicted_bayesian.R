ce39cc5e3463fe406d5597d81aa7b265e2e57fdc
Daniel <mail@danielluedecke.de>
2022-10-16 10:08:41 +0200

    get_predicted brms : iterations argument #670

--- R/get_predicted_bayesian.R
+++ R/get_predicted_bayesian.R
@@ -54,7 +54,6 @@ get_predicted.stanreg <- function(x,
   fun_args <- list(x,
     newdata = args$data,
     re.form = args$re.form,
-    ndraws = iterations,
     draws = iterations
   )
 
@@ -62,7 +61,7 @@ get_predicted.stanreg <- function(x,
   # as it got changed in recent brms versions (but we want to preserve compatibility)
   check_s3_method <- tryCatch(names(formals(utils::getS3method("posterior_predict", class(x)[1]))), error = function(e) NULL)
   if (!is.null(check_s3_method) && "nsamples" %in% check_s3_method) {
-    fun_args <- c(fun_args, nsamples = iterations)
+    fun_args <- c(fun_args, ndraws = iterations, nsamples = iterations)
   } else {
     fun_args <- c(fun_args, ndraws = iterations)
   }
