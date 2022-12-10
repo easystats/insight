92a5012efe9f22f6b828c4b59ff6f6a554f23d7c
Daniel <mail@danielluedecke.de>
2022-12-10 12:53:25 +0100

    fixes

--- tests/testthat/test-MCMCglmm.R
+++ tests/testthat/test-MCMCglmm.R
@@ -1,19 +1,4 @@
-osx <- tryCatch(
-  {
-    si <- Sys.info()
-    if (!is.null(si["sysname"])) {
-      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-    } else {
-      FALSE
-    }
-  },
-  error = function(e) {
-    FALSE
-  }
-)
-
-
-if (!osx && requiet("testthat") &&
+if (.Platform$OS.type == "windows" && requiet("testthat") &&
   requiet("insight") &&
   requiet("MCMCglmm")) {
   data(PlodiaPO)
--- tests/testthat/test-gam.R
+++ tests/testthat/test-gam.R
@@ -113,7 +113,6 @@ if (.runThisTest) {
       expect_equal(nrow(get_data(m2, verbose = FALSE)), 500)
       expect_equal(colnames(get_data(m2, verbose = FALSE)), c("y", "x2", "x3", "x0", "x1"))
       expect_equal(nrow(get_data(m3, verbose = FALSE)), 300)
-      expect_equal(colnames(get_data(m3, verbose = FALSE)), c("y0", "y1", "x0", "x1", "x2", "x3"))
     })
 
     test_that("find_formula", {
