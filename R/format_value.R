537b323af09385dd8510b5d192e7d2da8c7f686a
Daniel <mail@danielluedecke.de>
2022-12-23 14:17:32 +0100

    fix vectorized if

--- R/format_value.R
+++ R/format_value.R
@@ -181,13 +181,7 @@ format_percent <- function(x, ...) {
   if (is.numeric(x)) {
     if (isTRUE(.as_percent)) {
       need_sci <- (abs(100 * x) >= 1e+5 | (log10(abs(100 * x)) < -digits)) & x != 0 & !.zap_small
-      if (is.na(x)) {
-        x <- .missing
-      } else if (need_sci) {
-        x <- sprintf("%.*e%%", digits, 100 * x)
-      } else {
-        x <- sprintf("%.*f%%", digits, 100 * x)
-      }
+      x <- ifelse(is.na(x), missing, ifelse(need_sci, sprintf("%.*e", digits, x), sprintf("%.*f", digits, x))) # nolint
     } else {
       if (is.character(digits) && grepl("scientific", digits, fixed = TRUE)) {
         digits <- tryCatch(as.numeric(gsub("scientific", "", digits, fixed = TRUE)),
@@ -205,13 +199,7 @@ format_percent <- function(x, ...) {
         x <- as.character(signif(x, digits))
       } else {
         need_sci <- (abs(x) >= 1e+5 | (log10(abs(x)) < -digits)) & x != 0 & !.zap_small
-        if (is.na(x)) {
-          x <- .missing
-        } else if (need_sci) {
-          x <- sprintf("%.*e", digits, x)
-        } else {
-          x <- sprintf("%.*f", digits, x)
-        }
+        x <- ifelse(is.na(x), missing, ifelse(need_sci, sprintf("%.*e", digits, x), sprintf("%.*f", digits, x))) # nolint
       }
     }
   } else if (anyNA(x)) {
