45333ff2bb5ee630e33cf9b3990ed1db3ced768b
etiennebacher <etienne.bacher@protonmail.com>
2023-01-27 13:47:16 +0100

    quiet a few more messages

--- tests/testthat/test-get_priors.R
+++ tests/testthat/test-get_priors.R
@@ -7,7 +7,7 @@ if (run_stan && requiet("brms")) {
   data(mtcars)
   set.seed(123)
 
-  model <- brms::brm(mpg ~ wt, data = mtcars, seed = 1, refresh = 0)
+  model <- suppressMessages(brms::brm(mpg ~ wt, data = mtcars, seed = 1, refresh = 0))
   priors <- insight::get_priors(model)
 
   test_that("get_priors", {
