d66d56555a240b3ead5b9083362594def6abfb1f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-03-10 10:52:07 -0500

    find_offset.fixest fixup

--- R/find_offset.R
+++ R/find_offset.R
@@ -46,8 +46,11 @@ find_offset <- function(x) {
   # fixest sometimes returns a weird macro syntax instead of the real offset
   # if we have to implement too many model-specific workarounds, it may eventually be worth it to do S3
   # VAB: no test because I can only replicate in a weird {etwfe} example
-  if (inherits(x, "fixest") && (is.null(offset) || isTRUE(grepl("^\\.\\.", offset)))) {
-    offset <- clean_names(x[["model_info"]][["offset"]])
+  if (inherits(x, "fixest")) {
+    if (is.null(offset) || isTRUE(grepl("^\\.\\.", offset))) {
+      offset <- clean_names(x[["model_info"]][["offset"]])
+    }
+    offset <- sub("^~", "", offset)
   }
 
   offset
