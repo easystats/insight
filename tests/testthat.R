261958ae9557d169ab74fbf70d3a66da4c3f0921
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-18 09:26:09 +0100

    avoid test contamination with better names

--- tests/testthat.R
+++ tests/testthat.R
@@ -23,29 +23,12 @@ if (require("testthat")) {
     }
   )
 
-  solaris <- tryCatch(
-    {
-      if (!is.null(si["sysname"])) {
-        grepl("SunOS", si["sysname"], ignore.case = TRUE)
-      } else {
-        FALSE
-      }
-    },
-    error = function(e) {
-      FALSE
-    }
-  )
-
   # disable / enable if needed
   if (.Platform$OS.type == "unix" && is_dev_version) {
     Sys.setenv("RunAllinsightStanTests" = "yes")
   } else {
     Sys.setenv("RunAllinsightStanTests" = "no")
   }
-
-  # if (!osx && !solaris) {
-  #   test_check("insight")
-  # }
 
   test_check("insight")
 }
