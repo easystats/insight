30eb1a2687dfab176ae2241cb7d3d242352ea16a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-19 10:14:04 +0100

    Test cleanup (#738)
    * Check if MCMCglmm tests still fail on Ubuntu
    
    closes #684
    
    * avoid test contamination with better names
    
    * Update test-ivreg_AER.R
    
    * suppress S3 methods messages
    
    * fix?
    
    * skip on older version
    
    * use ivreg ns
    
    * skip on older version
    
    ---------
    
    Co-authored-by: Daniel <mail@danielluedecke.de>

--- tests/testthat/test-ivreg_AER.R
+++ tests/testthat/test-ivreg_AER.R
@@ -1,141 +1,146 @@
-if (skip_if_not_or_load_if_installed("AER")) {
-  data(CigarettesSW)
-  CigarettesSW$rprice <- with(CigarettesSW, price / cpi)
-  CigarettesSW$rincome <- with(CigarettesSW, income / population / cpi)
-  CigarettesSW$tdiff <- with(CigarettesSW, (taxs - tax) / cpi)
+skip_if_not_or_load_if_installed("AER")
 
-  cig_data <<- CigarettesSW
+data(CigarettesSW)
+CigarettesSW$rprice <- with(CigarettesSW, price / cpi)
+CigarettesSW$rincome <- with(CigarettesSW, income / population / cpi)
+CigarettesSW$tdiff <- with(CigarettesSW, (taxs - tax) / cpi)
 
-  m1 <- AER::ivreg(
-    log(packs) ~ log(rprice) + log(rincome) | log(rincome) + tdiff + I(tax / cpi),
-    data = cig_data,
-    subset = year == "1995"
+cig_data <<- CigarettesSW
+
+mod_aer_ivreg <- AER::ivreg(
+  log(packs) ~ log(rprice) + log(rincome) | log(rincome) + tdiff + I(tax / cpi),
+  data = cig_data,
+  subset = year == "1995"
+)
+
+test_that("model_info", {
+  expect_true(model_info(mod_aer_ivreg)$is_linear)
+})
+
+test_that("find_predictors", {
+  expect_identical(
+    find_predictors(mod_aer_ivreg),
+    list(
+      conditional = c("rprice", "rincome"),
+      instruments = c("rincome", "tdiff", "tax", "cpi")
+    )
   )
+  expect_identical(
+    find_predictors(mod_aer_ivreg, flatten = TRUE),
+    c("rprice", "rincome", "tdiff", "tax", "cpi")
+  )
+  expect_null(find_predictors(mod_aer_ivreg, effects = "random"))
+})
 
-  test_that("model_info", {
-    expect_true(model_info(m1)$is_linear)
-  })
+test_that("find_random", {
+  expect_null(find_random(mod_aer_ivreg))
+})
 
-  test_that("find_predictors", {
-    expect_identical(
-      find_predictors(m1),
-      list(
-        conditional = c("rprice", "rincome"),
-        instruments = c("rincome", "tdiff", "tax", "cpi")
-      )
+test_that("get_random", {
+  expect_warning(get_random(mod_aer_ivreg))
+})
+
+test_that("find_response", {
+  expect_identical(find_response(mod_aer_ivreg), "packs")
+})
+
+test_that("get_response", {
+  expect_equal(get_response(mod_aer_ivreg), cig_data$packs[cig_data$year == "1995"])
+})
+
+test_that("get_predictors", {
+  expect_equal(
+    colnames(get_predictors(mod_aer_ivreg)),
+    c("rprice", "rincome", "tdiff", "tax", "cpi")
+  )
+})
+
+test_that("link_inverse", {
+  expect_equal(link_inverse(mod_aer_ivreg)(0.2), 0.2, tolerance = 1e-5)
+})
+
+test_that("get_data", {
+  expect_equal(nrow(get_data(mod_aer_ivreg)), 48)
+  expect_equal(
+    colnames(get_data(mod_aer_ivreg)),
+    c("packs", "rprice", "rincome", "tdiff", "tax", "cpi", "year")
+  )
+})
+
+test_that("find_formula", {
+  expect_length(find_formula(mod_aer_ivreg), 2)
+  expect_equal(
+    find_formula(mod_aer_ivreg),
+    list(
+      conditional = as.formula("log(packs) ~ log(rprice) + log(rincome)"),
+      instruments = as.formula("~log(rincome) + tdiff + I(tax/cpi)")
+    ),
+    ignore_attr = TRUE
+  )
+})
+
+test_that("find_variables", {
+  expect_equal(
+    find_variables(mod_aer_ivreg),
+    list(
+      response = "packs",
+      conditional = c("rprice", "rincome"),
+      instruments = c("rincome", "tdiff", "tax", "cpi")
     )
-    expect_identical(
-      find_predictors(m1, flatten = TRUE),
-      c("rprice", "rincome", "tdiff", "tax", "cpi")
+  )
+  expect_equal(
+    find_variables(mod_aer_ivreg, flatten = TRUE),
+    c("packs", "rprice", "rincome", "tdiff", "tax", "cpi")
+  )
+})
+
+test_that("n_obs", {
+  expect_equal(n_obs(mod_aer_ivreg), 48)
+})
+
+test_that("linkfun", {
+  expect_false(is.null(link_function(mod_aer_ivreg)))
+})
+
+test_that("find_parameters", {
+  expect_equal(
+    find_parameters(mod_aer_ivreg),
+    list(
+      conditional = c("(Intercept)", "log(rprice)", "log(rincome)")
     )
-    expect_null(find_predictors(m1, effects = "random"))
-  })
+  )
+  expect_equal(nrow(get_parameters(mod_aer_ivreg)), 3)
+  expect_equal(
+    get_parameters(mod_aer_ivreg)$Parameter,
+    c("(Intercept)", "log(rprice)", "log(rincome)")
+  )
+})
 
-  test_that("find_random", {
-    expect_null(find_random(m1))
-  })
+test_that("is_multivariate", {
+  expect_false(is_multivariate(mod_aer_ivreg))
+})
 
-  test_that("get_random", {
-    expect_warning(get_random(m1))
-  })
-
-  test_that("find_response", {
-    expect_identical(find_response(m1), "packs")
-  })
-
-  test_that("get_response", {
-    expect_equal(get_response(m1), cig_data$packs[cig_data$year == "1995"])
-  })
-
-  test_that("get_predictors", {
-    expect_equal(
-      colnames(get_predictors(m1)),
-      c("rprice", "rincome", "tdiff", "tax", "cpi")
+test_that("find_terms", {
+  expect_equal(
+    find_terms(mod_aer_ivreg),
+    list(
+      response = "log(packs)",
+      conditional = c("log(rprice)", "log(rincome)"),
+      instruments = c("log(rincome)", "tdiff", "I(tax/cpi)")
     )
-  })
+  )
+  expect_equal(nrow(get_parameters(mod_aer_ivreg)), 3)
+  expect_equal(
+    get_parameters(mod_aer_ivreg)$Parameter,
+    c("(Intercept)", "log(rprice)", "log(rincome)")
+  )
+})
 
-  test_that("link_inverse", {
-    expect_equal(link_inverse(m1)(0.2), 0.2, tolerance = 1e-5)
-  })
+test_that("find_statistic", {
+  expect_identical(find_statistic(mod_aer_ivreg), "t-statistic")
+})
 
-  test_that("get_data", {
-    expect_equal(nrow(get_data(m1)), 48)
-    expect_equal(
-      colnames(get_data(m1)),
-      c("packs", "rprice", "rincome", "tdiff", "tax", "cpi", "year")
-    )
-  })
-
-  test_that("find_formula", {
-    expect_length(find_formula(m1), 2)
-    expect_equal(
-      find_formula(m1),
-      list(
-        conditional = as.formula("log(packs) ~ log(rprice) + log(rincome)"),
-        instruments = as.formula("~log(rincome) + tdiff + I(tax/cpi)")
-      ),
-      ignore_attr = TRUE
-    )
-  })
-
-  test_that("find_variables", {
-    expect_equal(
-      find_variables(m1),
-      list(
-        response = "packs",
-        conditional = c("rprice", "rincome"),
-        instruments = c("rincome", "tdiff", "tax", "cpi")
-      )
-    )
-    expect_equal(
-      find_variables(m1, flatten = TRUE),
-      c("packs", "rprice", "rincome", "tdiff", "tax", "cpi")
-    )
-  })
-
-  test_that("n_obs", {
-    expect_equal(n_obs(m1), 48)
-  })
-
-  test_that("linkfun", {
-    expect_false(is.null(link_function(m1)))
-  })
-
-  test_that("find_parameters", {
-    expect_equal(
-      find_parameters(m1),
-      list(
-        conditional = c("(Intercept)", "log(rprice)", "log(rincome)")
-      )
-    )
-    expect_equal(nrow(get_parameters(m1)), 3)
-    expect_equal(
-      get_parameters(m1)$Parameter,
-      c("(Intercept)", "log(rprice)", "log(rincome)")
-    )
-  })
-
-  test_that("is_multivariate", {
-    expect_false(is_multivariate(m1))
-  })
-
-  test_that("find_terms", {
-    expect_equal(
-      find_terms(m1),
-      list(
-        response = "log(packs)",
-        conditional = c("log(rprice)", "log(rincome)"),
-        instruments = c("log(rincome)", "tdiff", "I(tax/cpi)")
-      )
-    )
-    expect_equal(nrow(get_parameters(m1)), 3)
-    expect_equal(
-      get_parameters(m1)$Parameter,
-      c("(Intercept)", "log(rprice)", "log(rincome)")
-    )
-  })
-
-  test_that("find_statistic", {
-    expect_identical(find_statistic(m1), "t-statistic")
-  })
+# to avoid `Registered S3 methods overwritten by 'ivreg'` messages
+if (isNamespaceLoaded("AER")) {
+  unloadNamespace("AER")
 }
