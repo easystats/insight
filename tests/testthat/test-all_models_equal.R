d566ba126d159adb9fd71fae17aefa2b7f21cb81
etiennebacher <etienne.bacher@protonmail.com>
2023-04-07 14:38:38 +0200

    clean tests until "test-brms.R" (included)

--- tests/testthat/test-all_models_equal.R
+++ tests/testthat/test-all_models_equal.R
@@ -1,21 +1,21 @@
-if (skip_if_not_installed("lme4")) {
- 
-  data(sleepstudy, package = "lme4")
+skip_if_not_installed("lme4")
 
-  m1 <- lm(mpg ~ wt + cyl + vs, data = mtcars)
-  m2 <- lm(mpg ~ wt + cyl, data = mtcars)
-  m3 <- lmer(Reaction ~ Days + (1 | Subject), data = sleepstudy)
-  m4 <- glm(formula = vs ~ wt, family = binomial(), data = mtcars)
+data(sleepstudy, package = "lme4")
 
-  test_that("all_models_equal", {
-    expect_true(all_models_equal(m1, m2))
-    expect_false(all_models_equal(m1, m2, mtcars))
-    suppressMessages(expect_message(all_models_equal(m1, m2, mtcars, verbose = TRUE)))
-    expect_false(suppressMessages(all_models_equal(m1, m2, mtcars, verbose = TRUE)))
-    expect_false(all_models_equal(m1, m2, m3))
-    expect_message(expect_false(all_models_equal(m1, m4, m2, m3, verbose = TRUE)))
+m1 <- lm(mpg ~ wt + cyl + vs, data = mtcars)
+m2 <- lm(mpg ~ wt + cyl, data = mtcars)
+m3 <- lme4::lmer(Reaction ~ Days + (1 | Subject), data = sleepstudy)
+m4 <- glm(formula = vs ~ wt, family = binomial(), data = mtcars)
 
-    expect_true(is_model_supported(m1))
-    expect_false(is_model_supported(mtcars))
-  })
-}
+test_that("all_models_equal", {
+  expect_true(all_models_equal(m1, m2))
+  expect_false(all_models_equal(m1, m2, mtcars))
+  suppressMessages(expect_message(all_models_equal(m1, m2, mtcars, verbose = TRUE)))
+  expect_false(suppressMessages(all_models_equal(m1, m2, mtcars, verbose = TRUE)))
+  expect_false(all_models_equal(m1, m2, m3))
+  expect_message(expect_false(all_models_equal(m1, m4, m2, m3, verbose = TRUE)))
+
+  expect_true(is_model_supported(m1))
+  expect_false(is_model_supported(mtcars))
+})
+
