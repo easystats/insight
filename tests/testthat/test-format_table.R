f266d23f3440507b06dbefd00cf306aeac850969
Daniel <mail@danielluedecke.de>
2023-05-22 19:00:55 +0200

    fix

--- tests/testthat/test-format_table.R
+++ tests/testthat/test-format_table.R
@@ -1,6 +1,7 @@
 skip_if_offline()
 skip_on_os(c("mac", "linux", "solaris"))
 skip_if_not_installed("bayestestR")
+skip_if_not_installed("rstanarm")
 
 # test for bayesian models -----------------
 m1 <- insight::download_model("stanreg_glm_1")
