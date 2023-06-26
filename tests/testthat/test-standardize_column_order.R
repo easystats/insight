893353fa3af3b6b761c9027b35a180dd47749932
Daniel <mail@danielluedecke.de>
2023-06-26 10:39:49 +0200

    tests

--- tests/testthat/test-standardize_column_order.R
+++ tests/testthat/test-standardize_column_order.R
@@ -56,22 +56,28 @@ test_that("get_predicted", {
 })
 
 
-## FIXME: requires "cmdstanr" package?
+test_that("reorder columns BF", {
+  # brms_bf <- suppressWarnings(download_model("brms_bf_1"))
+  out <- data.frame(
+    Parameter = c("b_Intercept", "b_wt", "sigma"),
+    Component = c("conditional", "conditional", "sigma"),
+    Median = c(32.22175, -3.755645, 3.461165),
+    CI = c(0.95, 0.95, 0.95),
+    CI_low = c(27.2244525, -4.9688055, 2.6517275),
+    CI_high = c(35.75887, -2.21074025, 4.69652725),
+    pd = c(1, 1, 1),
+    ROPE_Percentage = c(0, 0, 0),
+    log_BF = c(14.4924732349718, 5.79962753110103, 8.89383915455679),
+    Rhat = c(1.00438747198895, 1.00100407213689, 0.992006699276081),
+    ESS = c(88.3152312142069, 91.7932788446396, 167.822262320689),
+    stringsAsFactors = FALSE
+  )
 
-# test_that("reorder columns BF", {
-#   skip_on_cran()
-#   skip_on_os(c("mac", "linux"))
-#   brms_bf <- suppressWarnings(download_model("brms_bf_1"))
-#   skip_if(is.null(brms_bf))
-#   skip_if_not_installed("parameters")
-#   skip_if_not_installed("bayestestR")
-#   out <- suppressWarnings(parameters::model_parameters(brms_bf, test = c("pd", "BF", "rope")))
-
-#   expect_named(
-#     standardize_column_order(out),
-#     c(
-#       "Parameter", "Median", "Component", "CI", "CI_low", "CI_high",
-#       "pd", "ROPE_Percentage", "log_BF", "Rhat", "ESS"
-#     )
-#   )
-# })
+  expect_named(
+    standardize_column_order(out),
+    c(
+      "Parameter", "Median", "Component", "CI", "CI_low", "CI_high",
+      "pd", "ROPE_Percentage", "log_BF", "Rhat", "ESS"
+    )
+  )
+})
