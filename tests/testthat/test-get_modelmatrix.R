4ac80a73a5daf4c960f9fe12a351c6a62cd6a432
Daniel <mail@danielluedecke.de>
2023-08-29 18:03:46 +0200

    tests

--- tests/testthat/test-get_modelmatrix.R
+++ tests/testthat/test-get_modelmatrix.R
@@ -5,11 +5,11 @@ test_that("Issue #612: factor padding", {
   # no factor
   mod <- glm(vs ~ cyl, data = mtcars, family = binomial)
   mm <- get_modelmatrix(mod)
-  expect_equal(nrow(mm), 32)
+  expect_identical(nrow(mm), 32L)
   mm <- get_modelmatrix(mod, data = mtcars)
-  expect_equal(nrow(mm), 32)
+  expect_identical(nrow(mm), 32L)
   mm <- get_modelmatrix(mod, data = head(mtcars))
-  expect_equal(nrow(mm), 6)
+  expect_identical(nrow(mm), 6L)
 
   # one factor
   dat <- mtcars
@@ -18,15 +18,15 @@ test_that("Issue #612: factor padding", {
 
   # no data argument
   mm <- get_modelmatrix(mod)
-  expect_equal(nrow(mm), 32)
+  expect_identical(nrow(mm), 32L)
 
   # enough factor levels
   mm <- get_modelmatrix(mod, data = head(dat))
-  expect_equal(nrow(mm), 6)
+  expect_identical(nrow(mm), 6L)
 
   # not enough factor levels
   mm <- get_modelmatrix(mod, data = dat[3, ])
-  expect_equal(nrow(mm), 1)
+  expect_identical(nrow(mm), 1L)
 })
 
 
@@ -48,7 +48,7 @@ test_that("get_modelmatrix - iv_robust", {
   out1 <- get_modelmatrix(x, data = get_datagrid(x, at = "P"))
   out2 <- model.matrix(terms(x), data = get_datagrid(x, at = "P", include_response = TRUE))
   expect_equal(out1, out2, tolerance = 1e-3, ignore_attr = TRUE)
-  expect_equal(nrow(get_datagrid(x, at = "P")), nrow(out2))
+  expect_identical(nrow(get_datagrid(x, at = "P")), nrow(out2))
 })
 
 
@@ -71,7 +71,7 @@ test_that("get_modelmatrix - ivreg", {
   out1 <- get_modelmatrix(x, data = get_datagrid(x, at = "P"))
   out2 <- model.matrix(terms(x), data = get_datagrid(x, at = "P", include_response = TRUE))
   expect_equal(out1, out2, tolerance = 1e-3, ignore_attr = TRUE)
-  expect_equal(nrow(get_datagrid(x, at = "P")), nrow(out2))
+  expect_identical(nrow(get_datagrid(x, at = "P")), nrow(out2))
 })
 
 
@@ -100,7 +100,7 @@ test_that("get_modelmatrix - lm_robust", {
   out1 <- get_modelmatrix(x, data = get_datagrid(x, at = "x"))
   out2 <- model.matrix(x, data = get_datagrid(x, at = "x", include_response = TRUE))
   expect_equal(out1, out2, tolerance = 1e-3, ignore_attr = TRUE)
-  expect_equal(nrow(get_datagrid(x, at = "x")), nrow(out2))
+  expect_identical(nrow(get_datagrid(x, at = "x")), nrow(out2))
 })
 
 
@@ -111,7 +111,7 @@ test_that("Issue #693", {
   x <- sample(1:3, n, replace = TRUE)
   w <- sample(1:4, n, replace = TRUE)
   y <- rnorm(n)
-  z <- ifelse(x + y + rlogis(n) > 1.5, 1, 0)
+  z <- as.numeric(x + y + rlogis(n) > 1.5)
   dat <<- data.frame(x = factor(x), w = factor(w), y = y, z = z)
   m <- glm(z ~ x + w + y, family = binomial, data = dat)
   nd <- head(dat, 2)
