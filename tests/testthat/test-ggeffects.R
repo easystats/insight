cb55addca4e2704de9d758d498d0f96a4692f5a7
Daniel <mail@danielluedecke.de>
2023-10-14 14:04:58 +0200

    fix

--- tests/testthat/test-ggeffects.R
+++ tests/testthat/test-ggeffects.R
@@ -69,7 +69,7 @@ test_that("get_statistic, hypothesis_test", {
   )
 
   param <- get_statistic(out)
-  expect_named(param, c("Sepal.Length", "Species", "Estimate"))
+  expect_named(param, c("Sepal.Length", "Species", "Statistic"))
   expect_identical(
     param$Sepal.Length,
     c("slope", "slope", "slope")
@@ -79,13 +79,13 @@ test_that("get_statistic, hypothesis_test", {
     structure(1:3, levels = c("setosa", "versicolor", "virginica"), class = "factor")
   )
   expect_equal(
-    param$Estimate,
-    c(0.79853, 0.31972, 0.23189),
+    param$Statistic,
+    c(7.23551, 4.24211, 3.79015),
     tolerance = 1e-3
   )
 
-  param <- get_parameters(out, merge_parameters = TRUE)
-  expect_named(param, c("Parameter", "Estimate"))
+  param <- get_statistic(out, merge_parameters = TRUE)
+  expect_named(param, c("Parameter", "Statistic"))
   expect_identical(
     param$Parameter,
     c(
@@ -94,25 +94,25 @@ test_that("get_statistic, hypothesis_test", {
     )
   )
   expect_equal(
-    param$Estimate,
-    c(0.79853, 0.31972, 0.23189),
+    param$Statistic,
+    c(7.23551, 4.24211, 3.79015),
     tolerance = 1e-3
   )
 
   out <- ggeffects::hypothesis_test(ggeffects::ggpredict(mgg, c("Sepal.Length", "Species")))
 
-  param <- get_parameters(out)
-  expect_named(param, c("Sepal.Length", "Species", "Estimate"))
+  param <- get_statistic(out)
+  expect_named(param, c("Sepal.Length", "Species", "Statistic"))
   expect_identical(
     param$Sepal.Length,
     c("slope", "slope", "slope")
   )
   expect_equal(
-    param$Estimate,
-    c(0.47881, 0.56664, 0.08783),
+    param$Statistic,
+    c(3.58262, 4.48915, 0.90475),
     tolerance = 1e-3
   )
 
-  param <- get_parameters(out, merge_parameters = TRUE)
-  expect_named(param, c("Parameter", "Estimate"))
+  param <- get_statistic(out, merge_parameters = TRUE)
+  expect_named(param, c("Parameter", "Statistic"))
 })
