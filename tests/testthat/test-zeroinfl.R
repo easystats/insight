3281cfeaca7f2e1449a0ebcc4763f85521426509
Daniel <mail@danielluedecke.de>
2024-01-31 09:12:15 +0100

    fix test

--- tests/testthat/test-zeroinfl.R
+++ tests/testthat/test-zeroinfl.R
@@ -11,8 +11,8 @@ test_that("model_info", {
 })
 
 test_that("n_parameters", {
-  expect_equal(n_parameters(m1), 8)
-  expect_equal(n_parameters(m1, component = "conditional"), 5)
+  expect_identical(n_parameters(m1), 8L)
+  expect_identical(n_parameters(m1, component = "conditional"), 5L)
 })
 
 test_that("find_predictors", {
@@ -39,9 +39,9 @@ test_that("link_inverse", {
 })
 
 test_that("get_data", {
-  expect_equal(nrow(get_data(m1)), 915)
-  expect_equal(
-    colnames(get_data(m1)),
+  expect_identical(nrow(get_data(m1)), 915L)
+  expect_named(
+    get_data(m1),
     c("art", "fem", "mar", "kid5", "ment", "phd")
   )
 })
@@ -59,7 +59,7 @@ test_that("find_formula", {
 })
 
 test_that("find_terms", {
-  expect_equal(
+  expect_identical(
     find_terms(m1),
     list(
       response = "art",
@@ -67,14 +67,14 @@ test_that("find_terms", {
       zero_inflated = c("kid5", "phd")
     )
   )
-  expect_equal(
+  expect_identical(
     find_terms(m1, flatten = TRUE),
     c("art", "fem", "mar", "kid5", "ment", "phd")
   )
 })
 
 test_that("n_obs", {
-  expect_equal(n_obs(m1), 915)
+  expect_identical(n_obs(m1), 915L)
 })
 
 test_that("linkfun", {
@@ -82,7 +82,7 @@ test_that("linkfun", {
 })
 
 test_that("find_parameters", {
-  expect_equal(
+  expect_identical(
     find_parameters(m1),
     list(
       conditional = c(
@@ -95,9 +95,9 @@ test_that("find_parameters", {
       zero_inflated = c("zero_(Intercept)", "zero_kid5", "zero_phd")
     )
   )
-  expect_equal(nrow(get_parameters(m1)), 8)
-  expect_equal(nrow(get_parameters(m1, component = "zi")), 3)
-  expect_equal(
+  expect_identical(nrow(get_parameters(m1)), 8L)
+  expect_identical(nrow(get_parameters(m1, component = "zi")), 3L)
+  expect_identical(
     get_parameters(m1)$Parameter,
     c(
       "count_(Intercept)",
@@ -135,7 +135,7 @@ test_that("get_statistic", {
 test_that("get_varcov", {
   # needs to be loaded
   suppressPackageStartupMessages({
-    suppressWarnings(suppressMessages(library(sandwich, quietly = TRUE, warn.conflicts = FALSE)))
+    suppressWarnings(suppressMessages(library(sandwich, quietly = TRUE, warn.conflicts = FALSE))) # nolint
   })
 
   set.seed(123)
@@ -146,21 +146,23 @@ test_that("get_varcov", {
 
   set.seed(123)
   vc1 <- get_varcov(m1, component = "conditional", vcov = "BS", vcov_args = list(R = 50))
-  count_col <- grepl("^count_", colnames(vc2))
+  count_col <- startsWith(colnames(vc2), "count_")
   expect_equal(vc1, vc2[count_col, count_col], ignore_attr = TRUE)
 
   set.seed(123)
   vc1 <- get_varcov(m1, component = "zero_inflated", vcov = "BS", vcov_args = list(R = 50))
-  zero_col <- grepl("^zero_", colnames(vc2))
+  zero_col <- startsWith(colnames(vc2), "zero_")
   expect_equal(vc1, vc2[zero_col, zero_col], ignore_attr = TRUE)
 })
 
+# fixes in calculation of test-statistic in pscl 1.5.9+
+skip_if(packageVersion("pscl") < "1.5.9")
 m2 <- pscl::zeroinfl(formula = art ~ . | 1, data = bioChemists, dist = "negbin")
 
 test_that("get_statistic", {
   expect_equal(
     get_statistic(m2)$Statistic,
-    c(1.84902, -2.97806, 1.83266, -3.32478, 0.42324, 8.38088, -0.14579),
+    c(1.8486, -2.97799, 1.83288, -3.32486, 0.42372, 8.38053, -0.21241),
     tolerance = 1e-3
   )
   expect_equal(
