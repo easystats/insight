f0c04708cc599f881bf91e82cedcba323b045bcd
Daniel <mail@danielluedecke.de>
2024-06-05 10:20:47 +0200

    fix

--- R/utils_compact.R
+++ R/utils_compact.R
@@ -12,19 +12,22 @@ compact_list <- function(x, remove_na = FALSE) {
   # iterate over elements of the list. we need to check
   # whether we have data frames
   if (!is.null(x) && length(x) && is.list(x)) {
-    x <- lapply(x, function(element) {
-      # for data frames, check if columns are vctrs.
-      # if so, remove the vctrs-class attributes
-      if (!is.null(element) && length(element) && is.data.frame(element)) {
-        element[] <- lapply(element, function(i) {
-          # remove vctr-class attributes in data frames
-          class(i) <- setdiff(class(i), c("haven_labelled", "vctrs_vctr"))
-          i
-        })
-      }
-      element
-    })
+    # data frame is also a "list" - sanity check here
+    # and remove attributes directly
+    if (is.data.frame(x)) {
+      x <- .remove_vctrs_class(x)
+    } else {
+      x <- lapply(x, function(element) {
+        # for data frames, check if columns are vctrs.
+        # if so, remove the vctrs-class attributes
+        if (!is.null(element) && length(element) && is.data.frame(element)) {
+          element <- .remove_vctrs_class(element)
+        }
+        element
+      })
+    }
   }
+  # finally, we can compact the list...
   if (remove_na) {
     x[!sapply(x, function(i) !is_model(i) && !inherits(i, c("Formula", "gFormula")) && (length(i) == 0L || is.null(i) || (length(i) == 1L && is.na(i)) || all(is.na(i)) || any(i == "NULL", na.rm = TRUE)))]
   } else {
@@ -47,4 +50,14 @@ compact_list <- function(x, remove_na = FALSE) {
 #' @export
 compact_character <- function(x) {
   x[!sapply(x, function(i) !nzchar(i, keepNA = TRUE) || all(is.na(i)) || any(i == "NULL", na.rm = TRUE))]
+}
+
+
+.remove_vctrs_class <- function(x) {
+  x[] <- lapply(x, function(i) {
+    # remove vctr-class attributes in data frames
+    class(i) <- setdiff(class(i), c("haven_labelled", "vctrs_vctr"))
+    i
+  })
+  x
 }
