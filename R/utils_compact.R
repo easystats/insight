0e6b167d09163b0c96ffdb0fd703145326b9ec71
Daniel <mail@danielluedecke.de>
2024-06-05 10:23:40 +0200

    refactor

--- R/utils_compact.R
+++ R/utils_compact.R
@@ -9,24 +9,8 @@
 #' compact_list(c(1, NA, NA), remove_na = TRUE)
 #' @export
 compact_list <- function(x, remove_na = FALSE) {
-  # iterate over elements of the list. we need to check
-  # whether we have data frames
-  if (!is.null(x) && length(x) && is.list(x)) {
-    # data frame is also a "list" - sanity check here
-    # and remove attributes directly
-    if (is.data.frame(x)) {
-      x <- .remove_vctrs_class(x)
-    } else {
-      x <- lapply(x, function(element) {
-        # for data frames, check if columns are vctrs.
-        # if so, remove the vctrs-class attributes
-        if (!is.null(element) && length(element) && is.data.frame(element)) {
-          element <- .remove_vctrs_class(element)
-        }
-        element
-      })
-    }
-  }
+  # to make regular(!) R code work with vctrs, we need to remove the vctrs-class
+  x <- .clean_from_vctrs(x)
   # finally, we can compact the list...
   if (remove_na) {
     x[!sapply(x, function(i) !is_model(i) && !inherits(i, c("Formula", "gFormula")) && (length(i) == 0L || is.null(i) || (length(i) == 1L && is.na(i)) || all(is.na(i)) || any(i == "NULL", na.rm = TRUE)))]
@@ -50,6 +34,31 @@ compact_list <- function(x, remove_na = FALSE) {
 #' @export
 compact_character <- function(x) {
   x[!sapply(x, function(i) !nzchar(i, keepNA = TRUE) || all(is.na(i)) || any(i == "NULL", na.rm = TRUE))]
+}
+
+
+# helper ----------------------------------------------------------------------
+
+.clean_from_vctrs <- function(x) {
+  # iterate over elements of the list. we need to check
+  # whether we have data frames
+  if (!is.null(x) && length(x) && is.list(x)) {
+    # data frame is also a "list" - sanity check here
+    # and remove attributes directly
+    if (is.data.frame(x)) {
+      x <- .remove_vctrs_class(x)
+    } else {
+      x <- lapply(x, function(element) {
+        # for data frames, check if columns are vctrs.
+        # if so, remove the vctrs-class attributes
+        if (!is.null(element) && length(element) && is.data.frame(element)) {
+          element <- .remove_vctrs_class(element)
+        }
+        element
+      })
+    }
+  }
+  x
 }
 
 
