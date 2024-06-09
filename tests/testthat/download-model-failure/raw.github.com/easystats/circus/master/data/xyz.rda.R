db549ca55eaf3d06954b83230b58bd640c33d9ee
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-06-09 11:47:00 +0200

    Implement `download_model()` using `{httr2}` and add tests (#885)
    * Use `{httr2}` instead of `{httr}`
    
    * add tests
    
    * be consistent in pattern
    
    * ignore lint
    
    * use httptest2 conditionally

---
+++ tests/testthat/download-model-failure/raw.github.com/easystats/circus/master/data/xyz.rda.R
@@ -0,0 +1,21 @@
+# nolint start
+# styler: off
+structure(list(
+  method = "GET", url = "https://raw.githubusercontent.com/easystats/circus/master/data/xyz.rda",
+  status_code = 404L, headers = structure(list(
+    `content-security-policy` = "default-src 'none'; style-src 'unsafe-inline'; sandbox",
+    `strict-transport-security` = "max-age=31536000", `x-content-type-options` = "nosniff",
+    `x-frame-options` = "deny", `x-xss-protection` = "1; mode=block",
+    `content-type` = "text/plain; charset=utf-8", `x-github-request-id` = "63EC:2A5B9F:A27173:ADAE50:66655C62",
+    `accept-ranges` = "bytes", date = "Sun, 09 Jun 2024 07:40:19 GMT",
+    via = "1.1 varnish", `x-served-by` = "cache-muc13972-MUC",
+    `x-cache` = "MISS", `x-cache-hits` = "0", `x-timer` = "S1717918819.042740,VS0,VE145",
+    vary = "Authorization,Accept-Encoding,Origin", `access-control-allow-origin` = "*",
+    `cross-origin-resource-policy` = "cross-origin", `x-fastly-request-id` = "e629ba1c490defdad14a749303c5f1649289f72c",
+    expires = "Sun, 09 Jun 2024 07:45:19 GMT", `source-age` = "0",
+    `content-length` = "14"
+  ), class = "httr2_headers"), body = charToRaw("404: Not Found"),
+  cache = new.env(parent = emptyenv())
+), class = "httr2_response")
+# styler: on
+# nolint end
---
+++ tests/testthat/test-download_model.R
@@ -0,0 +1,22 @@
+skip_if_not_installed("httptest2")
+
+httptest2::with_mock_dir("download-model-success", {
+  test_that("we can successfully get existing model", {
+    model <- download_model("lm_0", verbose = FALSE)
+    expect_s3_class(model, "lm")
+  })
+})
+
+httptest2::with_mock_dir("download-model-failure", {
+  test_that("we fail gracefully while getting non-existing model", {
+    model <- download_model("xyz", verbose = FALSE)
+    expect_null(model)
+  })
+})
+
+test_that("request fails gracefully without internet", {
+  httptest2::without_internet({
+    model <- download_model("aov_1", verbose = FALSE)
+    expect_null(model)
+  })
+})
