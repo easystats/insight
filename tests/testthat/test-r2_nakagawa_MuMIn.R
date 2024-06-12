fd1af65b847cbca82099eb6527dbce88b2fd404b
Daniel <mail@danielluedecke.de>
2024-06-12 14:22:40 +0200

    tests

--- tests/testthat/test-r2_nakagawa_MuMIn.R
+++ tests/testthat/test-r2_nakagawa_MuMIn.R
@@ -18,15 +18,48 @@ test_that("glmmTMB, linear", {
   expect_equal(out1$R2, out2$R2, tolerance = 1e-4)
   expect_equal(out1$R2_adjusted, out2$R2_adjusted, tolerance = 1e-1)
 
-  # linear, no ranom slope
+  # linear, no random slope
   m <- glmmTMB::glmmTMB(Reaction ~ Days + (1 | Subject), data = sleepstudy)
   out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
   out2 <- performance::r2_nakagawa(m)
   expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
   expect_equal(out1[, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-4)
 
-  # linear, with ranom slope
+  # linear, no random slope, inverse
+  m <- glmmTMB::glmmTMB(
+    Reaction ~ Days + (1 | Subject),
+    data = sleepstudy,
+    family = gaussian("inverse")
+  )
+  out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
+  out2 <- performance::r2_nakagawa(m)
+  expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out1[, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-4)
+
+  # linear, with random slope
   m <- glmmTMB::glmmTMB(Reaction ~ Days + (1 + Days | Subject), data = sleepstudy)
+  out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
+  out2 <- performance::r2_nakagawa(m)
+  expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out1[, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-4)
+
+  # linear, random slope, inverse
+  m <- glmmTMB::glmmTMB(
+    Reaction ~ Days + (1 + Days | Subject),
+    data = sleepstudy,
+    family = gaussian("inverse")
+  )
+  out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
+  out2 <- performance::r2_nakagawa(m)
+  expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out1[, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-4)
+
+  # linear, random slope, log
+  m <- glmmTMB::glmmTMB(
+    Reaction ~ Days + (1 + Days | Subject),
+    data = sleepstudy,
+    family = gaussian("log")
+  )
   out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
   out2 <- performance::r2_nakagawa(m)
   expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
@@ -37,14 +70,14 @@ test_that("glmmTMB, linear", {
 test_that("lme4, linear", {
   data(sleepstudy, package = "lme4")
 
-  # linear, no ranom slope
+  # linear, no random slope
   m <- lme4::lmer(Reaction ~ Days + (1 | Subject), data = sleepstudy)
   out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
   out2 <- performance::r2_nakagawa(m)
   expect_equal(out1[, "R2m"], out2$R2_marginal, ignore_attr = TRUE, tolerance = 1e-4)
   expect_equal(out1[, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-4)
 
-  # linear, with ranom slope
+  # linear, with random slope
   m <- lme4::lmer(Reaction ~ Days + (1 + Days | Subject), data = sleepstudy)
   out1 <- suppressWarnings(MuMIn::r.squaredGLMM(m))
   out2 <- performance::r2_nakagawa(m)
