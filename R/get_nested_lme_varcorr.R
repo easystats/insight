84b80768d70ae2fb3cd8d99cf2d0da97d8ecfca1
Daniel <mail@danielluedecke.de>
2024-06-13 09:44:48 +0200

    add comment

--- R/get_nested_lme_varcorr.R
+++ R/get_nested_lme_varcorr.R
@@ -8,6 +8,7 @@
   vcor <- lme4::VarCorr(x)
   class(vcor) <- "matrix"
 
+  ## FIXME: doesn't work for nested RE from MASS::glmmPQL, see Nakagawa example
   re_index <- (which(rownames(vcor) == "(Intercept)") - 1)[-1]
   vc_list <- split(data.frame(vcor, stringsAsFactors = FALSE), findInterval(seq_len(nrow(vcor)), re_index))
   vc_rownames <- split(rownames(vcor), findInterval(seq_len(nrow(vcor)), re_index))
