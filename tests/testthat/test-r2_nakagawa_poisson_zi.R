9fd98e22b539506c7de4babdc5cc292d9060682f
Daniel <mail@danielluedecke.de>
2024-06-18 15:54:16 +0200

    fix test

--- tests/testthat/test-r2_nakagawa_poisson_zi.R
+++ tests/testthat/test-r2_nakagawa_poisson_zi.R
@@ -28,8 +28,8 @@ test_that("glmmTMB, Poisson zero-inflated", {
 
   # full model
   out <- performance::r2_nakagawa(m)
-  expect_equal(out2$R2_marginal, 0.4636197, ignore_attr = TRUE, tolerance = 1e-4)
-  expect_equal(out2$R2_conditional, 0.5751936, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out$R2_marginal, 0.2923215, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out$R2_conditional, 0.362671, ignore_attr = TRUE, tolerance = 1e-4)
 
   # glmmTMB, sqrt, no random slope -------------------------------------------------
   m <- glmmTMB::glmmTMB(count ~ mined + (1 | site),
@@ -56,6 +56,12 @@ test_that("glmmTMB, Poisson zero-inflated", {
   # expect_equal(out1[2, "R2c"], out2$R2_conditional, ignore_attr = TRUE, tolerance = 1e-1)
   expect_equal(out2$R2_marginal, 0.524714, ignore_attr = TRUE, tolerance = 1e-1)
   expect_equal(out2$R2_conditional, 0.6498465, ignore_attr = TRUE, tolerance = 1e-1)
+
+  # full model
+  out <- suppressWarnings(performance::r2_nakagawa(m))
+  expect_equal(out$R2_marginal, 0.3344432, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out$R2_conditional, 0.4142003, ignore_attr = TRUE, tolerance = 1e-4)
+
 
   # glmmTMB, sqrt, random slope -------------------------------------------------
   m <- suppressWarnings(glmmTMB::glmmTMB(count ~ mined + cover + (1 + cover | site),
