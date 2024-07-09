d346f9527c3b3eb45283118f44b902806b9b7cd7
Daniel <mail@danielluedecke.de>
2024-07-09 10:06:50 +0200

    add test

---
+++ tests/testthat/test-r2_nakagawa_ordered_beta.R
@@ -0,0 +1,25 @@
+skip_on_cran()
+
+skip_if_not_installed("glmmTMB", minimum_version = "1.1.8")
+skip_if_not_installed("performance")
+
+
+# ==============================================================================
+# ordered beta mixed models, glmmTMB
+# ==============================================================================
+
+test_that("glmmTMB, ordbeta", {
+  # dataset ---------------------------------
+  skip_if_not_installed("datawizard")
+  skip_if_not_installed("lme4")
+  data(sleepstudy, package = "lme4")
+  sleepstudy$y <- datawizard::normalize(sleepstudy$Reaction)
+  m <- glmmTMB::glmmTMB(
+    y ~ Days + (Days | Subject),
+    data = sleepstudy,
+    family = glmmTMB::ordbeta()
+  )
+  out <- suppressWarnings(performance::r2_nakagawa(m, verbose = FALSE))
+  expect_equal(out$R2_marginal, 0.2799715, ignore_attr = TRUE, tolerance = 1e-4)
+  expect_equal(out$R2_conditional, 0.8504158, ignore_attr = TRUE, tolerance = 1e-4)
+})
