aeee5bd2ce4926f9127a8145d78c3104cb1e52df
Daniel <mail@danielluedecke.de>
2024-07-10 23:09:18 +0200

    Fix test

--- tests/testthat/test-r2_nakagawa_ordered_beta.R
+++ tests/testthat/test-r2_nakagawa_ordered_beta.R
@@ -2,24 +2,28 @@ skip_on_cran()
 
 skip_if_not_installed("glmmTMB", minimum_version = "1.1.8")
 skip_if_not_installed("performance")
+skip_if_not_installed("withr")
 
 
 # ==============================================================================
 # ordered beta mixed models, glmmTMB
 # ==============================================================================
 
-test_that("glmmTMB, ordbeta", {
-  # dataset ---------------------------------
-  skip_if_not_installed("datawizard")
-  skip_if_not_installed("lme4")
-  data(sleepstudy, package = "lme4")
-  sleepstudy$y <- datawizard::normalize(sleepstudy$Reaction)
-  m <- glmmTMB::glmmTMB(
-    y ~ Days + (Days | Subject),
-    data = sleepstudy,
-    family = glmmTMB::ordbeta()
-  )
-  out <- suppressWarnings(performance::r2_nakagawa(m, verbose = FALSE))
-  expect_equal(out$R2_marginal, 0.2799715, ignore_attr = TRUE, tolerance = 1e-4)
-  expect_equal(out$R2_conditional, 0.8504158, ignore_attr = TRUE, tolerance = 1e-4)
-})
+withr::with_environment(
+  new.env(),
+  test_that("glmmTMB, ordbeta", {
+    # dataset ---------------------------------
+    skip_if_not_installed("datawizard")
+    skip_if_not_installed("lme4")
+    data(sleepstudy, package = "lme4")
+    sleepstudy$y <- datawizard::normalize(sleepstudy$Reaction)
+    m <- glmmTMB::glmmTMB(
+      y ~ Days + (Days | Subject),
+      data = sleepstudy,
+      family = glmmTMB::ordbeta()
+    )
+    out <- suppressWarnings(performance::r2_nakagawa(m, verbose = FALSE))
+    expect_equal(out$R2_marginal, 0.2799715, ignore_attr = TRUE, tolerance = 1e-4)
+    expect_equal(out$R2_conditional, 0.8504158, ignore_attr = TRUE, tolerance = 1e-4)
+  })
+)
