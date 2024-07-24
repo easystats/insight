17726fa597a4eefac5a8d591f3148090667b37ad
Daniel <mail@danielluedecke.de>
2024-07-24 11:32:32 +0200

    fix test

--- tests/testthat/test-get_variance.R
+++ tests/testthat/test-get_variance.R
@@ -36,9 +36,10 @@ v5 <- suppressWarnings(get_variance(fm5))
 v6 <- suppressWarnings(get_variance(fm6))
 
 test_that("error for non-mixed", {
+  skip_if_not_installed("glmmTMB")
   data(mtcars)
   expect_error(
-    get_variance(lm(mpg ~ gear, data = mtcars)),
+    get_variance(glmmTMB::glmmTMB(mpg ~ gear, data = mtcars)),
     regex = "This function only works for mixed models"
   )
 })
