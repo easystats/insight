8753dca7c30d103c215d2ae5f7a4dff6a1a0fbee
Daniel <mail@danielluedecke.de>
2024-11-04 23:06:03 +0100

    style

--- R/format_value.R
+++ R/format_value.R
@@ -112,8 +112,8 @@ format_value.numeric <- function(x,
                                  style_negative = "hyphen",
                                  ...) {
   # check input
-  style_positive <- match.arg(style_positive, choices = c("none", "plus", "space"))
-  style_negative <- match.arg(style_negative, choices = c("hyphen", "minus", "parens"))
+  style_positive <- validate_argument(style_positive, c("none", "plus", "space"))
+  style_negative <- validate_argument(style_negative, c("hyphen", "minus", "parens"))
 
   if (protect_integers) {
     out <- .format_value_unless_integer(
@@ -224,7 +224,13 @@ format_percent <- function(x, ...) {
 
 
 
-.format_value <- function(x, digits = 2, .missing = "", .width = NULL, .as_percent = FALSE, .zap_small = FALSE, ...) {
+.format_value <- function(x,
+                          digits = 2,
+                          .missing = "",
+                          .width = NULL,
+                          .as_percent = FALSE,
+                          .zap_small = FALSE,
+                          ...) {
   # proper character NA
   if (is.na(.missing)) .missing <- NA_character_
 
@@ -248,9 +254,7 @@ format_percent <- function(x, ...) {
       }
     } else if (is.character(digits) && grepl("scientific", digits, fixed = TRUE)) {
       digits <- tryCatch(
-        expr = {
-          as.numeric(gsub("scientific", "", digits, fixed = TRUE))
-        },
+        as.numeric(gsub("scientific", "", digits, fixed = TRUE)),
         error = function(e) {
           5
         }
@@ -259,9 +263,7 @@ format_percent <- function(x, ...) {
       x <- sprintf("%.*e", digits, x)
     } else if (is.character(digits) && grepl("signif", digits, fixed = TRUE)) {
       digits <- tryCatch(
-        expr = {
-          as.numeric(gsub("signif", "", digits, fixed = TRUE))
-        },
+        as.numeric(gsub("signif", "", digits, fixed = TRUE)),
         error = function(e) {
           NA
         }
