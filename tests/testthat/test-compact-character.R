ec728355e2439d6478438ba6c738d908c032b17c
Daniel <mail@danielluedecke.de>
2024-11-08 22:13:15 +0100

    lintr

--- tests/testthat/test-compact-character.R
+++ tests/testthat/test-compact-character.R
@@ -1,10 +1,10 @@
 test_that("compact character works as expected", {
-  expect_equal(compact_character(c("x", "y", NA)), c("x", "y"))
-  expect_equal(compact_character(c("x", "y", NA_real_)), c("x", "y"))
-  expect_equal(compact_character(c("x", "NULL", "", "y")), c("x", "y"))
+  expect_identical(compact_character(c("x", "y", NA)), c("x", "y"))
+  expect_identical(compact_character(c("x", "y", NA_real_)), c("x", "y"))
+  expect_identical(compact_character(c("x", "NULL", "", "y")), c("x", "y"))
 
   # scalar
-  expect_equal(compact_character(""), character(0))
+  expect_identical(compact_character(""), character(0))
 
   # date
   x <- as.Date(c("1900-05-06", "", "1900-05-07", NA))
