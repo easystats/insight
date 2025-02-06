ce9e7ba64fd3929f051bce61eca3d471ee2491da
Daniel <mail@danielluedecke.de>
2025-02-06 16:00:43 +0100

    Prepare CRAN release (#1000)
    * Prepare CRAN release
    
    * fix
    
    * version
    
    * fix
    
    * update rbuildignore

--- tests/testthat/test-ggeffects.R
+++ tests/testthat/test-ggeffects.R
@@ -4,58 +4,34 @@ test_that("get_parameters, hypothesis_test", {
   data(iris)
   mgg <- lm(Sepal.Width ~ Sepal.Length * Species, data = iris)
 
-  out <- ggeffects::hypothesis_test(
-    ggeffects::ggpredict(mgg, c("Sepal.Length", "Species")),
-    test = NULL
-  )
+  out <- ggeffects::test_predictions(mgg, "Sepal.Length", by = "Species")
 
   param <- get_parameters(out)
-  expect_named(param, c("Sepal.Length", "Species", "Estimate"))
+  expect_named(param, c("Level1", "Level2", "Estimate"))
   expect_identical(
-    param$Sepal.Length,
-    c("slope", "slope", "slope")
-  )
-  expect_identical(
-    param$Species,
-    structure(1:3, levels = c("setosa", "versicolor", "virginica"), class = "factor")
+    as.character(param$Level1),
+    c("versicolor", "virginica", "virginica")
   )
   expect_equal(
     param$Estimate,
-    c(0.79853, 0.31972, 0.23189),
+    c(-0.47881, -0.56664, -0.08783),
     tolerance = 1e-3
   )
 
   param <- get_parameters(out, merge_parameters = TRUE)
   expect_named(param, c("Parameter", "Estimate"))
   expect_identical(
-    param$Parameter,
+    as.character(param$Parameter),
     c(
-      "Sepal.Length [slope], Species [setosa]", "Sepal.Length [slope], Species [versicolor]",
-      "Sepal.Length [slope], Species [virginica]"
+      "Level1 [versicolor], Level2 [setosa]", "Level1 [virginica], Level2 [setosa]",
+      "Level1 [virginica], Level2 [versicolor]"
     )
   )
   expect_equal(
     param$Estimate,
-    c(0.79853, 0.31972, 0.23189),
+    c(-0.47881, -0.56664, -0.08783),
     tolerance = 1e-3
   )
-
-  out <- ggeffects::hypothesis_test(ggeffects::ggpredict(mgg, c("Sepal.Length", "Species")))
-
-  param <- get_parameters(out)
-  expect_named(param, c("Sepal.Length", "Species", "Estimate"))
-  expect_identical(
-    param$Sepal.Length,
-    c("slope", "slope", "slope")
-  )
-  expect_equal(
-    param$Estimate,
-    c(0.47881, 0.56664, 0.08783),
-    tolerance = 1e-3
-  )
-
-  param <- get_parameters(out, merge_parameters = TRUE)
-  expect_named(param, c("Parameter", "Estimate"))
 })
 
 
@@ -63,56 +39,32 @@ test_that("get_statistic, hypothesis_test", {
   data(iris)
   mgg <- lm(Sepal.Width ~ Sepal.Length * Species, data = iris)
 
-  out <- ggeffects::hypothesis_test(
-    ggeffects::ggpredict(mgg, c("Sepal.Length", "Species")),
-    test = NULL
-  )
+  out <- ggeffects::hypothesis_test(mgg, "Sepal.Length", by = "Species")
 
   param <- get_statistic(out)
-  expect_named(param, c("Sepal.Length", "Species", "Statistic"))
-  expect_identical(
-    param$Sepal.Length,
-    c("slope", "slope", "slope")
-  )
-  expect_identical(
-    param$Species,
-    structure(1:3, levels = c("setosa", "versicolor", "virginica"), class = "factor")
-  )
+  expect_named(param, c("Level1", "Level2", "Statistic"))
   expect_equal(
     param$Statistic,
-    c(7.23551, 4.24211, 3.79015),
+    c(-3.58238, -4.48999, -0.90468),
     tolerance = 1e-3
+  )
+  expect_identical(
+    as.character(param$Level1),
+    c("versicolor", "virginica", "virginica")
   )
 
   param <- get_statistic(out, merge_parameters = TRUE)
   expect_named(param, c("Parameter", "Statistic"))
   expect_identical(
-    param$Parameter,
+    as.character(param$Parameter),
     c(
-      "Sepal.Length [slope], Species [setosa]", "Sepal.Length [slope], Species [versicolor]",
-      "Sepal.Length [slope], Species [virginica]"
+      "Level1 [versicolor], Level2 [setosa]", "Level1 [virginica], Level2 [setosa]",
+      "Level1 [virginica], Level2 [versicolor]"
     )
   )
   expect_equal(
     param$Statistic,
-    c(7.23551, 4.24211, 3.79015),
+    c(-3.58238, -4.48999, -0.90468),
     tolerance = 1e-3
   )
-
-  out <- ggeffects::hypothesis_test(ggeffects::ggpredict(mgg, c("Sepal.Length", "Species")))
-
-  param <- get_statistic(out)
-  expect_named(param, c("Sepal.Length", "Species", "Statistic"))
-  expect_identical(
-    param$Sepal.Length,
-    c("slope", "slope", "slope")
-  )
-  expect_equal(
-    param$Statistic,
-    c(3.58262, 4.48915, 0.90475),
-    tolerance = 1e-3
-  )
-
-  param <- get_statistic(out, merge_parameters = TRUE)
-  expect_named(param, c("Parameter", "Statistic"))
 })
