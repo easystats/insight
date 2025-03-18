271499f10593b7a3fd3bd2a7da4beba57181dab4
Daniel <mail@danielluedecke.de>
2025-03-18 13:40:37 +0100

    docs

--- R/fish.R
+++ R/fish.R
@@ -5,3 +5,11 @@
 #'
 #' @description A sample data set, used in tests and some examples.
 NULL
+
+#' @docType data
+#' @title Sample data set
+#' @name efc_insight
+#' @keywords data
+#'
+#' @description A sample data set, internally used in tests.
+NULL
