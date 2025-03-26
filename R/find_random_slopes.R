f529539e1ece117945d2b2c0f01d10c28a35625b
Daniel <mail@danielluedecke.de>
2025-03-26 23:47:49 +0100

    random fx

--- R/find_random_slopes.R
+++ R/find_random_slopes.R
@@ -27,7 +27,7 @@ find_random_slopes <- function(x) {
 
   # for brms, we can have random effects for auxilliary elements, too
   if (inherits(x, "brmsfit")) {
-    components <- c(components, paste0(.brms_aux_elements(), "_random"))
+    components <- c(components, paste0(names(forms$pforms), "_random"))
   }
   # for glmmTMB, we can have random effects for dispersion component, too
   if (inherits(x, "glmmTMB")) {
