498f557e19ad08b3a561aed92a5edc2f04aed1c0
Daniel <mail@danielluedecke.de>
2025-03-27 09:19:38 +0100

    fix

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -226,14 +226,16 @@
 
 .all_elements <- function() {
   c(
-    "conditional", "conditional1", "conditional2", "conditional3", "precision",
-    "nonlinear", "random", "zi", "zero_inflated", "zero_inflated_random", "shape",
-    "dispersion", "dispersion_random", "instruments", "interactions", "simplex",
-    "smooth_terms", "sigma", "nu", "tau", "correlation", "slopes", "cluster",
-    "extra", "scale", "marginal", "alpha", "beta", "survival", "infrequent_purchase",
-    "auxiliary", "mix", "shiftprop", "phi", "ndt", "hu", "xi", "coi", "zoi",
-    "aux", "dist", "selection", "outcome", "time_dummies", "sigma_random",
-    "beta_random", "car", "nominal", "bidrange", "mu", "kappa", "bias"
+    "alpha", "aux", "auxiliary", "beta", "beta_random", "bias", "bs",
+    "bidrange", "car", "cluster", "coi", "conditional", "conditional1",
+    "conditional2", "conditional3", "correlation", "delta", "dispersion",
+    "dispersion_random", "dist", "extra", "hu", "infrequent_purchase",
+    "instruments", "interactions", "k", "kappa", "marginal", "mix", "mu",
+    "ndt", "nominal", "nonlinear", "nu", "outcome", "phi", "precision",
+    "random", "scale", "selection", "shape", "shiftprop", "sigma",
+    "sigma_random", "simplex", "slopes", "smooth_terms", "survival",
+    "tau", "time_dummies", "xi", "zero_inflated", "zero_inflated_random",
+    "zi", "zoi"
   )
 }
 
