88a52bf5ad2a95b9de024ec0cc4025621e9a9f62
Daniel <mail@danielluedecke.de>
2025-04-12 00:59:11 +0200

    add test

--- tests/testthat/test-print_parameters.R
+++ tests/testthat/test-print_parameters.R
@@ -4,6 +4,7 @@ skip_if_offline()
 skip_on_os("mac")
 skip_if_not_installed("brms")
 skip_if_not_installed("httr2")
+skip_if_not_installed("glmmTMB")
 
 test_that("print_parameters brms", {
   m <- download_model("brms_zi_3")
@@ -29,6 +30,38 @@ test_that("print_parameters brms", {
       `random.zi.Intercept: persons` = c("# Random effects (zero-inflated)", "blue"),
       `random.conditional.SD/Cor: persons` = c("# Random effects (conditional)", "blue"),
       `random.zi.SD/Cor: persons` = c("# Random effects (zero-inflated)", "blue")
+    )
+  )
+})
+
+
+test_that("print_parameters glmmTMB", {
+  data(fish, package = "insight")
+  m <- suppressWarnings(glmmTMB::glmmTMB(
+    count ~ child + camper + (1 + xb | persons),
+    ziformula = ~ child + livebait + (1 + zg + nofish | ID),
+    dispformula = ~xb,
+    data = fish,
+    family = poisson()
+  ))
+  out <- print_parameters(m)
+  expect_named(
+    out,
+    c(
+      "fixed.conditional", "fixed.zero_inflated", "random.zero_inflated.ID",
+      "random.conditional.persons"
+    )
+  )
+  att <- lapply(out, function(i) {
+    attributes(i)$table_caption
+  })
+  expect_identical(
+    att,
+    list(
+      fixed.conditional = c("# Fixed effects (conditional)", "blue"),
+      fixed.zero_inflated = c("# Fixed effects (zero-inflated)", "blue"),
+      random.zero_inflated.ID = c("# Random effects (zero-inflated)", "blue"),
+      random.conditional.persons = c("# Random effects (conditional)", "blue")
     )
   )
 })
