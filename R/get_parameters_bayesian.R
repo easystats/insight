27c1de6184fcf5608de2a68159ca99ad11452836
Daniel <mail@danielluedecke.de>
2025-05-02 13:02:09 +0200

    same again

--- R/get_parameters_bayesian.R
+++ R/get_parameters_bayesian.R
@@ -227,6 +227,9 @@ get_parameters.stanreg <- function(x,
                                    summary = FALSE,
                                    centrality = "mean",
                                    ...) {
+  # we require namespace, to ensure that `as.data.frame()` works correctly
+  check_if_installed("rstanarm")
+
   dots <- list(...)
   # allow hidden argument "variable" to be passed directly to as.data.frame
   if (is.null(dots$variable)) {
