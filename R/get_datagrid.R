66978bcf924d4f1ada6a0956875e7e9a71875b64
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-23 07:59:50 -0400

    marginaleffects minimum_version

--- R/get_datagrid.R
+++ R/get_datagrid.R
@@ -929,6 +929,7 @@ get_datagrid.emm_list <- function(x, ...) {
 
 #' @export
 get_datagrid.slopes <- function(x, ...) {
+  check_if_installed("marginaleffects", minimum_version = "0.29.0")
   nd <- marginaleffects::components(x, "newdata")
   cols_newdata <- colnames(nd)
   cols_contrast <- colnames(x)[grep("^contrast_?", colnames(x))]
