718d194ba23a96c15db778bab6332c895825f0b7
Daniel <mail@danielluedecke.de>
2026-04-14 09:22:43 +0200

    fix

--- tests/testthat/test-get_simulated.R
+++ tests/testthat/test-get_simulated.R
@@ -425,7 +425,11 @@ test_that("get_simulated - mgcv", {
   set.seed(2) ## simulate some data...
   dat <- mgcv::gamSim(1, n = 400, dist = "normal", scale = 2)
   b <- mgcv::gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat)
-  out <- get_simulated(b, data = get_datagrid(b, "x1"), iterations = 5)
+  out <- get_simulated(
+    b,
+    data = get_datagrid(b, "x1", include_response = TRUE),
+    iterations = 5
+  )
   expect_identical(dim(out), c(10L, 5L))
 
   skip_if_not_installed("modelbased")
