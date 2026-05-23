7c0c121fd8cc91a0e0a74b119643201583cfe9c0
Daniel <mail@danielluedecke.de>
2026-05-23 16:03:42 +0200

    fixes

--- R/utils_compact.R
+++ R/utils_compact.R
@@ -9,45 +9,27 @@
 #' compact_list(c(1, NA, NA), remove_na = TRUE)
 #' @export
 compact_list <- function(x, remove_na = FALSE) {
-  # recursive compact_list() nested lists
-  if (is.list(x)) {
-    x <- lapply(x, function(i) {
-      if (
-        is.list(i) &&
-          !is.data.frame(i) &&
-          !is_model(i) &&
-          !inherits(i, c("Formula", "gFormula")) &&
-          !is.function(i)
-      ) {
-        return(compact_list(i, remove_na = remove_na))
-      }
-      i
-    })
-  }
-
   is_remove <- vapply(
     x,
     function(i) {
-      if (is_model(i) || inherits(i, c("Formula", "gFormula")) || is.function(i)) {
+      if (is_model(i)) {
+        return(FALSE)
+      }
+      if (inherits(i, c("Formula", "gFormula"))) {
+        return(FALSE)
+      }
+      if (is.function(i)) {
         return(FALSE)
       }
       if (remove_na) {
-        if (is.atomic(i) && all(is.na(i))) {
-          return(TRUE)
-        } else if (.safe(all(is.na(i)), FALSE)) {
-          return(TRUE)
-        }
-      } else if (length(i) == 0L || is.null(i)) {
-        # Because of the recursive compact_list step above, list(NULL, NULL) is
-        # now list(), which triggers length(i) == 0L.
-        return(TRUE)
+        all(is.na(i)) || .is_null_string(i)
+      } else {
+        (length(i) == 0L || is.null(i) || .is_null_string(i))
       }
-      .is_null_string(i)
     },
-    FUN.VALUE = logical(1),
+    logical(1),
     USE.NAMES = FALSE
   )
-
   x[!is_remove]
 }
 
@@ -88,8 +70,62 @@ compact_character <- function(x) {
     return(FALSE)
   }
   if (is.list(object)) {
-    # recursion on nested lists
-    return(any(rapply(object, .is_null_string, how = "unlist")))
+    if (.large_list_depth(object) > 1) {
+      return(FALSE)
+    } else {
+      return(.safe(
+        any(unlist(as.character(object), use.names = FALSE) == "NULL", na.rm = TRUE),
+        FALSE
+      ))
+    }
   }
   .safe(any(as.character(object) == "NULL", na.rm = TRUE), FALSE)
 }
+
+
+.large_list_depth <- function(x, depth = 0) {
+  if (!is.list(x) || is.data.frame(x) || depth > 1) {
+    return(depth)
+  } else {
+    return(max(vapply(x, .large_list_depth, FUN.VALUE = numeric(1), depth = depth + 1)))
+  }
+}
+
+# # recursive compact_list() nested lists
+# if (is.list(x)) {
+#   x <- lapply(x, function(i) {
+#     if (
+#       is.list(i) &&
+#         !is.data.frame(i) &&
+#         !is_model(i) &&
+#         !inherits(i, c("Formula", "gFormula")) &&
+#         !is.function(i)
+#     ) {
+#       return(compact_list(i, remove_na = remove_na))
+#     }
+#     i
+#   })
+# }
+
+# is_remove <- vapply(
+#   x,
+#   function(i) {
+#     if (is_model(i) || inherits(i, c("Formula", "gFormula")) || is.function(i)) {
+#       return(FALSE)
+#     }
+#     if (remove_na) {
+#       if (is.atomic(i) && all(is.na(i))) {
+#         return(TRUE)
+#       } else if (.safe(all(is.na(i)), FALSE)) {
+#         return(TRUE)
+#       }
+#     } else if (length(i) == 0L || is.null(i)) {
+#       # Because of the recursive compact_list step above, list(NULL, NULL) is
+#       # now list(), which triggers length(i) == 0L.
+#       return(TRUE)
+#     }
+#     .is_null_string(i)
+#   },
+#   FUN.VALUE = logical(1),
+#   USE.NAMES = FALSE
+# )
